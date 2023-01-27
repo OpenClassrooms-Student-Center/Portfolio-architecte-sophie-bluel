e536b436fba366f525a05f7ba2c4b8688eee1985
studentsjj <marielouise04@hotmail.fr>
2023-01-27 17:20:55 +0100

    feat: filter button

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,13 +1,29 @@
-async function getWorks () {
+async function getWorks() {
     const reponse = await fetch("http://localhost:5678/api/works");
     const data = await reponse.json();
 
     for(let work of data) {
         document.querySelector(".gallery").innerHTML += `<figure>
-                                                        <img src=${work.imageUrl} alt="Hotel First Arte - New Delhi" crossorigin="anonymous">
+                                                        <img src=${work.imageUrl} alt="" crossorigin="anonymous">
                                                         <figcaption>${work.title}</figcaption>
                                                         </figure>`
     }
 }
-getWorks ();
+getWorks();
+
+
+async function getCategories() {
+    const reponse =await fetch("http://localhost:5678/api/categories");
+    const data = await reponse.json();
+    for(let categorie of data){
+            document.querySelector(".filtres").innerHTML += `<button class="categories-btn" data-id="${categorie.id}">${categorie.name}</button>`
+            }
+            const filterButton = document.querySelector(".filtres");
+            filterButton.addEventListener("click",function () {
+                console.log("hello");
+                document.querySelector(".gallery").innerHTML ="";
+                getWorks();
+            });
+}
+getCategories();
 
