221da33871d454a6441390701d29974637599b7a
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-02-23 16:51:17 +0100

    récupération des noms avec .map

--- FrontEnd/projet_deco.js
+++ FrontEnd/projet_deco.js
@@ -28,6 +28,51 @@ function genererProjets(){
         figure.appendChild(figcaption);
 
         divGallery.appendChild(figure)
-    
     }
 }
+
+const boutonTous = document.querySelector(".tous");
+boutonTous.addEventListener("click", function () {
+    const filtreTous = projets.filter(function (projet) {
+        return projet.title !== null;
+    });
+console.log(filtreTous);
+});
+
+
+const boutonObjets = document.querySelector(".objets");
+boutonObjets.addEventListener("click", function () {
+    const filtreObjets = projets.filter(function (projet) {
+        return projets.name === "Objets";
+    });
+console.log(Category.name);
+});
+
+const boutonAppartements = document.querySelector(".appartements");
+boutonAppartements.addEventListener("click", function () {
+    const filtreAppartements = projets.filter(function (projet) {
+        return projet.title === null;
+    });
+console.log(filtreAppartements);
+});
+
+const boutonHotelsRestaurants = document.querySelector(".hotelsrestaurants");
+boutonHotelsRestaurants.addEventListener("click", function () {
+    const filtreHotelsRestaurants = projets.filter(function (projet) {
+        return projet.title !== null;
+    });
+console.log(filtreHotelsRestaurants);
+});
+
+
+
+
+// catégorie des projets
+const categorieDeProjet = await fetch("http://localhost:5678/api/categories");
+console.log(categorieDeProjet);
+
+const nomDesCategories = await categorieDeProjet.json();
+console.log(nomDesCategories)
+
+const noms = nomDesCategories.map(nomDeCategorie => nomDeCategorie.name);
+console.log(noms);
