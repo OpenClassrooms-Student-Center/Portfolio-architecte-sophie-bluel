c2b2256e0a02917e981692a07323f4408cb72f2d
unknown <sarah.sarahlea1998@gmail.com>
2023-03-03 16:10:07 +0100

    code commented + fixed filterDiv stylying on index page

--- FrontEnd/assets/js/categories.js
+++ FrontEnd/assets/js/categories.js
@@ -1,27 +1,48 @@
 import { renderWorks } from "./works.js";
 export function renderFilters() {
+  //Grâce à  fetch on va aller recuperer les donnes dans categories
   fetch("http://localhost:5678/api/categories")
     .then((response) => response.json())
+    //Ensuite 
     .then((categories) => {
+      //On va d'abord créer un bouton "Tous" pour une catégorie qui n'existe pas dans la liste des categories
       const filterDiv = document.createElement("div");
+      filterDiv.className = "filterDiv";
       const allElementsButton = document.createElement("button");
       allElementsButton.innerText = "Tous";
+      //On insere le btn dans la div filterDiv
       filterDiv.appendChild(allElementsButton);
+      //On fait une boucle pour que chaque category dans categories ait un bouton créé 
       categories.forEach((category) => {
+        //On crée donc un button 
         const newButton = document.createElement("button");
+        //On ajoute du texte a l'interieur du btn qui prendra le nom de la categorie 
+        //category.name permet d'acceder a l'attribut "name" dans category qui possède ici le nom de category
         newButton.innerText = category.name;
-        newButton.id = category.id
+        //On ajoute un attribut id au btn qui est identique à celui de la category
+        // 
+        newButton.id = category.id;
+        //On va également inserer ces btns dans la même div que celui de "tous" => filterDiv
         filterDiv.appendChild(newButton);
+        //On ajoute un event listener aux btns pour "ecouter" a chaque fois que le user click sur un des btns
+        //lors d'un click on effectuera la fonction renderWorks(category.name)
         newButton.addEventListener("click", function () {
-          renderWorks(category.name)
-        });
 
+          //On appelle la fonction renderWorks dans le fichier works.js
+          //cette fonction affiche les works 
+          //elle prend comme paramettre une category
+          renderWorks(category.name);
+          //Ici on a pris category.name comme paramettre qui va nous permettre d'afficher uniquement les works qui correspondent
+          //a "l'innerText" du bouton
+          // Ex : si user click sur le btn "Objects" le eventListener ecoute, voit que c'est innerText "Objects" va le mettre en parametre
+          //de la fonction renderWorks
+        });
       });
       const gallery = document.querySelector(".gallery");
       gallery.parentNode.insertBefore(filterDiv, gallery);
 
       allElementsButton.addEventListener("click", function () {
-        renderWorks("Tous")
+        renderWorks("Tous");
       });
     });
 }
--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -11,9 +11,13 @@ export function renderWorks(category) {
     .then((works) => {
       //pour chaque objet work dans works alors on va ...
       works.forEach((work) => {
-        //créer une figure + img en html
+       
         console.log(work)
+        
+        //On va aller voir pour chaque category si elle correspond a dans l'objet work la valeur de l'objet cateogry et son atrribut name
+        //vu qu'on est dans une boucle pour chaque work on va faire cette comparaison sur tous les objets work 
         if (category == work.category.name){
+          //creation des elements html necessaires pour accueilir la reponse des works
           const newFigure = document.createElement("figure");
           const newImage = document.createElement("img");
           //Assigner des attributs  (src et alt) aux instances des constantes qui vinnent d'etre crééer ( newImage)
@@ -24,9 +28,13 @@ export function renderWorks(category) {
           const newFigcaption = document.createElement("figcaption");
           //On assigne la valeur contenue dans l'attribut "title" a la figcaption
           newFigcaption.innerText = work.title;
+          //On insere la figcaption dans la figure avec la fonction appendChild
           newFigure.appendChild(newFigcaption);
           //On réinsere le tout(la figure complète) dans la gallerie
           gallery.appendChild(newFigure);
+
+          //Dans le cas ou la category est egale a "tous" qui n'est pas dans la liste des categories et qui à été créée
+          //On va afficher tous les works peu importe leur catégorie
         } else if (category == "Tous"){
 
         const newFigure = document.createElement("figure");
