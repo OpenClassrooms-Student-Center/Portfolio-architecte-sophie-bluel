f7cb4d10aa4184267ad4e10b17ec3455b5cd0417
Amar <a.zirouel@gmail.com>
2023-03-16 19:24:58 +0100

    Create filter

--- FrontEnd/scripts/app.js
+++ FrontEnd/scripts/app.js
@@ -1,8 +1,29 @@
 let works = []
+let categories = new Set()
 
 init()
 
 async function init() {
     works = [...(await getWorks())]
     createGallery(works)
+    createCategories()
+    createCategoriesButtons(categories, works)
+}
+
+const createCategories = () => {
+    works.forEach((work) => {
+        categories.add(work.category.name)
+    })
+}
+
+const filterByCategory = (category) => {
+    const filteredWorks = works.filter((work) => work.category.name === category)
+    createGallery(filteredWorks)
+}
+
+const removeGreenBackground = () => {
+    const categoriesButtons = document.querySelectorAll('.category-button')
+    categoriesButtons.forEach((button) => {
+        button.classList.remove('bg-green')
+    })
 }
--- FrontEnd/scripts/models.js
+++ FrontEnd/scripts/models.js
@@ -12,7 +12,36 @@ const createPictureCard = (title, imageUrl) => {
 
 const createGallery = (works) => {
     const galleryContainer = document.querySelector('.gallery')
+    galleryContainer.innerHTML = ''
     works.forEach((work) => {
         galleryContainer.appendChild(createPictureCard(work.title, work.imageUrl))
+    })
+}
+
+const createCategoryButton = (name) => {
+    const categoryButton = document.createElement('button')
+    categoryButton.classList.add('category-button')
+    categoryButton.innerHTML = name
+    categoryButton.addEventListener('click', (event) => {
+        removeGreenBackground()
+        categoryButton.classList.add('bg-green')
+        filterByCategory(name)
+    })
+    return categoryButton
+}
+
+const createCategoriesButtons = (categories, works) => {
+    const categoriesContainer = document.querySelector('.categories')
+    const allCategoryButton = document.createElement('button')
+    allCategoryButton.classList.add('category-button')
+    allCategoryButton.innerHTML = 'Tous'
+    allCategoryButton.addEventListener('click', (event) => {
+        removeGreenBackground()
+        allCategoryButton.classList.add('bg-green')
+        createGallery(works)
+    })
+    categoriesContainer.appendChild(allCategoryButton)
+    categories.forEach((category) => {
+        categoriesContainer.appendChild(createCategoryButton(category))
     })
 }
