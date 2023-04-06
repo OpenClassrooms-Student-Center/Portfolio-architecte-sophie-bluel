0dbe3d6c4fac3f68d21df200fe78d9e6e02c1467
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-04-06 15:26:40 +0200

    Issue 1.2
    Création des buttons <li> avec JS suivant table API categories
    Amélioration du code de cnx API
    
    work on Filter Element

--- FrontEnd/js/fonctions/api.js
+++ FrontEnd/js/fonctions/api.js
@@ -1,8 +1,14 @@
+/**
+ * Connexion avec API
+ * @param {*} url 
+ * @param {*} options 
+ * @returns 
+ */
 export async function fetchJSON(url, options = {}) {
     const headers = { Accepte: 'application/json', ...options.headers }
     const r = await fetch(url, { ...options, headers })
     if (r.ok) {
         return r.json();
     }
-    throw new Error('Erreur serveur', { cause: r });
+    throw new Error('Erreur serveur', { cause: "Il y a un problème avec la connexion de API" });
 }
--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -1,35 +1,89 @@
 /*
 Mes Projets
  */
-
 // Mes Imports (type="module")
 import { fetchJSON } from "./fonctions/api.js";
 import { createElement } from "./fonctions/dom.js";
 
 
-try {
-    const maListWorks = await fetchJSON("http://localhost:5678/api/works");
+/*
+LES PROJETS - Récuperation de tous les Projets 
+ */
+// 
+const maListWorks = await fetchJSON("http://localhost:5678/api/works", {
+    // si je souhaite rajouter des options.
+});
 
-    // Je crée tous les elements suivant le nombre de réponses trouvés
-    for (let item = 0; item < maListWorks.length; item++) {
-        // Je crée les balises suivant ma fonction
-        const figureElement = createElement("figure");
-        const imgElement = createElement("img");
-        const captionElement = createElement("figcaption");
-        // J'assigne la valeur dans mon nouveau element crée
-        imgElement.src = maListWorks[item].imageUrl;
-        captionElement.innerText = maListWorks[item].title;
-        // Je declare la balise parent Ref!
-        const contenerWorks = document.querySelector(".gallery");
-        // Je les inbrique et affiche
-        contenerWorks.appendChild(figureElement);
-        figureElement.appendChild(imgElement);
-        figureElement.appendChild(captionElement);
-    };
+/*
+LES CATEGORIES - Récuperation de toutes les categories
+ */
+// 
+const maListCategories = await fetchJSON("http://localhost:5678/api/categories", {
+    // si je souhaite rajouter des options.
+});
 
-} catch (error) {
-    console.error(error);
+
+/*
+1/ AFFICHER LES PROJETS <INIT>
+ */
+// Je crée tous les elements suivant le nombre de réponses trouvés
+for (let item = 0; item < maListWorks.length; item++) {
+    // Je crée les balises suivant ma fonction c'est plus facile d'intégrer des para comme des class !
+    const figureElement = createElement("figure");
+    const imgElement = createElement("img", {
+        "src": maListWorks[item].imageUrl
+    });
+    const captionElement = createElement("figcaption", {
+    });
+    // J'assigne la valeur dans mon nouveau element crée
+    captionElement.innerText = maListWorks[item].title;
+    // Je declare la balise parent Ref!
+    const contenerWorks = document.querySelector(".gallery");
+    // Je les inbrique et affiche
+    contenerWorks.appendChild(figureElement);
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
 };
 
 
 
+
+// console.log(maListCategories);
+
+/*
+__FILTERS <BUTTON>
+ */
+// 1) I make the first button <ALL>
+const myFirstFilter = createElement("li", {
+    "class": "filter__item",
+    "data-id": "Tout"
+})
+// Value to my button
+myFirstFilter.innerText = "Tout";
+// Parent's to my new elements
+const contenerFilter = document.querySelector(".filter");
+// i insert my new element to index.html
+contenerFilter.appendChild(myFirstFilter);
+
+// 2) i made all other filters from API
+for (let category = 0; category < maListCategories.length; category++) {
+    const itemFilter = createElement("li", {
+        "class": "filter__item",
+        "data-id": maListCategories[category].name
+    });
+    // Value to my button
+    itemFilter.innerText = maListCategories[category].name;
+    // Parent's to my new elements
+    const contenerFilter = document.querySelector(".filter");
+    // i insert my new element to index.html
+    contenerFilter.appendChild(itemFilter);
+}
+
+
+// FAIRE UN FILTRE SUIVANT LE CLICK DU BOUTON ET EN RECHERCHANT AVEC UN DATA-ID
+const boutonFilter = document.querySelector(".filter__item");
+boutonFilter.addEventListener("click", function () {
+
+
+
+});
