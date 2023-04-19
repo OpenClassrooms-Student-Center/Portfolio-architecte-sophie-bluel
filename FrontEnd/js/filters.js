29446882f1bd8931cb0fa8b9d93c61cf625a5076
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-04-19 11:45:10 +0200

    Issue 2.2_authentification
    localstorage token ok
    redirection page ok
    nettoyage code works & filters ok
    control cnx avec modification button login ok
    
    TERMINEE

--- FrontEnd/js/filters.js
+++ FrontEnd/js/filters.js
@@ -1,32 +1,68 @@
 // Mes Imports (type="module")
+import { fetchJSON } from "./fonctions/api.js";
+import { renderWorks } from "./works.js";
 import { createElement } from "./fonctions/dom.js";
+import { errorMessage } from "./fonctions/dom.js";
 
+export async function renderFilters() {
+    try {
+        const liste = await fetchJSON("http://localhost:5678/api/categories");
 
-export function renderFilters(liste) {
-    // 1) I make the first button <ALL>
-    const buttonAll = createElement("li", {
-        "class": "filter__item filter__item--select",
-        "id": "filterBtn_0",
-        "data-id": 0
-    },
-        "Tout");
-    // Parent's to my new elements
-    const contenerFilter = document.querySelector(".filter");
-    // i insert my new element to index.html
-    contenerFilter.appendChild(buttonAll);
-
-    // 2) i made all other filters from API
-    liste.forEach(item => {
-        const buttonsFilter = createElement("li", {
-            "class": "filter__item",
-            "id": "filterBtn_" + item.id,
-            "data-id": item.id
+        // 1) I make the first button <ALL>
+        const buttonAll = createElement("li", {
+            "class": "filter__item filter__item--select",
+            "id": "filterBtn_0",
+            "data-id": 0
         },
-            item.name
-        );
+            "Tout");
+        // Parent's to my new elements
         const contenerFilter = document.querySelector(".filter");
-        contenerFilter.appendChild(buttonsFilter);
-    });
+        // i insert my new element to index.html
+        contenerFilter.appendChild(buttonAll);
+
+        // 2) i made all other filters from API
+        liste.forEach(item => {
+            const buttonsFilter = createElement("li", {
+                "class": "filter__item",
+                "id": "filterBtn_" + item.id,
+                "data-id": item.id
+            },
+                item.name
+            );
+            const contenerFilter = document.querySelector(".filter");
+            contenerFilter.appendChild(buttonsFilter);
+        });
+
+
+        // onCllick
+        const boutonsFilter = document.querySelectorAll(".filter__item");
+        boutonsFilter.forEach(element => {
+            element.addEventListener("click", async function () {
+                const categoryId = element.dataset.id;
+                // take off class --select to all button
+                boutonsFilter.forEach((button) => {
+                    button.classList.remove("filter__item--select");
+                });
+                // reinitialise page
+                document.querySelector(".gallery").innerHTML = "";
+                // Add class --select
+                element.classList.add("filter__item--select");
+                if (categoryId == 0) {
+                    return renderWorks();
+                }
+                const maListe = await fetchJSON("http://localhost:5678/api/works");
+                const filterByObjets = maListe.filter(function (objet) {
+                    return objet.category.id == categoryId;
+                });
+                renderWorks(filterByObjets);
+            });
+        });
+
+    } catch (error) {
+        console.error(error);
+        const message = "Il y a une erreur sur le fetch des projets triés par catégorie !";
+        errorMessage(message, "#portfolio");
+    };
 
 };
 
--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,94 +1,79 @@
-import { fetchJSON } from "./fonctions/api.js";
 import { errorMessage, errorMessageRemove } from "./fonctions/dom.js";
-
-/**
- * IDEE
- * 
- * 1/ recuperation des informations dans le formulaire et creation d'un objet => userLogin
- * 
- * 2/ je fetchPOST userLogin
- * 
- * 3/ verifier la réponse de API 
- *      a/ r.status = 401 => Not Authorized (mauvais mot de passe) => message utilisateur
- *      b/ r.status = 404 => User not found (mauvais utilisateur) => message utilisateur
- *      c/ r.status = 200 => Connected => (next)
- * 
- * 4/ je stocke la token dans le SESSION STORAGE
- * 
- * 5/ redirection sur index.html
- * 
- * 6/ mode EDITE !!!!
- * 
- * 
- */
-
 
 
 /*
 GET LOGIN
  */
-const formLogin = document.querySelector("#formLogin");
+getLogin();
 
-formLogin.addEventListener("submit", function (event) {
-    // stop form method
-    event.preventDefault();
+export function getLogin() {
 
-    // Delete error message
-    errorMessageRemove("main");
-    const elementEmail = document.querySelector("#mail");
-    elementEmail.classList.remove("wrong");
-    const elementPass = document.querySelector("#pass");
-    elementPass.classList.remove("wrong");
+    const formLogin = document.querySelector("#formLogin");
 
-    // control umpty imput
-    const email = event.target.querySelector("#mail").value;
-    if (email === "") {
-        alert("Formulaire non completé !");
-        elementEmail.classList.add("wrong");
-        return;
-    };
+    formLogin.addEventListener("submit", async function (event) {
 
-    const passWord = event.target.querySelector("#pass").value;
-    if (passWord === "") {
-        alert("Formulaire non completé !");
-        elementPass.classList.add("wrong");
-        return;
-    };
+        // stop form method
+        event.preventDefault();
 
-    const user = {
-        "email": email,
-        "password": passWord,
-    }
+        // Delete error message
+        errorMessageRemove("main");
+        const elementEmail = document.querySelector("#mail");
+        elementEmail.classList.remove("wrong");
+        const elementPass = document.querySelector("#pass");
+        elementPass.classList.remove("wrong");
 
-    const cnx = fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-        .then((r) => {
-            if (r.ok) {
-                return r.json();
-            }
-            if (r.status === 401 || r.status === 404) {
-                const message = "Il y a une erreur sur les identifiants !";
-                errorMessage(message, "main");
-                return r.json();
-            }
-            throw new Error('Erreur serveur', { cause: "Il y a un problème avec la connexion de API" });
+        // control umpty imput
+        const email = event.target.querySelector("#mail").value;
+        if (email === "") {
+            alert("Formulaire non completé !");
+            elementEmail.classList.add("wrong");
+            return;
+        };
+
+        const passWord = event.target.querySelector("#pass").value;
+        if (passWord === "") {
+            alert("Formulaire non completé !");
+            elementPass.classList.add("wrong");
+            return;
+        };
+
+        const user = {
+            "email": email,
+            "password": passWord,
+        }
+
+        const cnx = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
         })
-        .then((data) => {
-            return console.log(data);
-            // Save resultat dans sessionstorage !!!!
-        });
+            .then((r) => {
+                if (r.ok) {
+                    return r.json();
+                }
+                if (r.status === 401 || r.status === 404) {
+                    const message = "Il y a une erreur sur les identifiants !";
+                    errorMessage(message, "main");
+                    return r.json();
+                }
+                throw new Error('Erreur serveur', { cause: "Il y a un problème avec la connexion de API" });
+            })
+            .then((data) => {
+                localStorage.setItem("SESSION", data.token);
+                window.location.href = './index.html';
+            })
+            .catch((e) => {
+                console.log(e);
+                const message = "Il y a une erreur sur url de l'api";
+                errorMessage(message, "main");
+            });
 
-});
+    });
 
-
-
-
+};
 
 
 
