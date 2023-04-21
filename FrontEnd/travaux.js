8600fdb3b322fb7e20bb24ada454566c21a93634
Arthur Jaquier <arthurjaquier81@gmail.com>
2023-04-21 18:34:17 +0200

    finalisation des filtres

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,21 +1,68 @@
 
 
-const reponse = await fetch(`http://localhost:5678/api/works`);
-const pieces = await reponse.json();
+await fetch(`http://localhost:5678/api/works`)
+.then(reponse => reponse.json())
+.then(pieces => {
+    document.querySelector(".gallery").innerHTML = "";
+    
+    function genererPieces(pieces){
+        for(let i = 0; i < pieces.length; i++){
+        const sectionFiches = document.querySelector(".gallery");
+        const piecesElement = document.createElement("figure");
+        
+        const nomElement = document.createElement("p");
+        nomElement.innerText = pieces[i].title;
+        const imageElement = document.createElement("img");
+        imageElement.src = pieces[i].imageUrl;
+        
+        sectionFiches.appendChild(piecesElement);
+        piecesElement.appendChild(imageElement);
+        piecesElement.appendChild(nomElement);
+        }
+        }
+        
+        genererPieces(pieces)
 
-document.querySelector(".gallery").innerHTML = "";
+        const buttonAll = document.querySelector(".button__all");
 
-for(let i = 0; i < pieces.length; i++){
-const sectionFiches = document.querySelector(".gallery");
-const piecesElement = document.createElement("figure");
+        buttonAll.addEventListener("click", function() {
+            const piecesFiltreesAll = pieces.filter(function(pieces) {
+                return pieces;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererPieces(piecesFiltreesAll);
+        });
+        
+        const buttonObject = document.querySelector(".button__objet");
+        
+        buttonObject.addEventListener("click", function() {
+            const piecesFiltreesObject = pieces.filter(function(piece) {
+                return piece.categoryId === 1;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererPieces(piecesFiltreesObject);
+        });
+        
+        const buttonFlat = document.querySelector(".button__flat");
+        
+        buttonFlat.addEventListener("click", function() {
+            const piecesFiltreesFlat = pieces.filter(function(piece) {
+                return piece.categoryId === 2;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererPieces(piecesFiltreesFlat);
+        });
+        
+        const buttonHostel = document.querySelector(".button__hostel");
+        
+        buttonHostel.addEventListener("click", function() {
+            const piecesFiltreesHostel = pieces.filter(function(piece) {
+                return piece.categoryId === 3;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererPieces(piecesFiltreesHostel);
+        });
 
-const nomElement = document.createElement("p");
-nomElement.innerText = pieces[i].title;
-const imageElement = document.createElement("img");
-imageElement.src = pieces[i].imageUrl;
+})
 
 
-sectionFiches.appendChild(piecesElement);
-piecesElement.appendChild(imageElement);
-piecesElement.appendChild(nomElement);
-}
