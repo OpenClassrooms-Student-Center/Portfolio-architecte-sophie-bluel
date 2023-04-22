3e407d06178a51592374d49924be3704d5614a32
Arthur Jaquier <arthurjaquier81@gmail.com>
2023-04-22 11:17:31 +0200

    récup des travaux + filtres + création HTML

---
+++ FrontEnd/login.js
@@ -0,0 +1,8 @@
+
+function ajoutListenerLogin() {
+    const formulaireLogin = document.querySelector(".form__login");
+    formulaireLogin.addEventListener("submit", function (event) {
+        event.preventDefault();
+        
+    })
+}
