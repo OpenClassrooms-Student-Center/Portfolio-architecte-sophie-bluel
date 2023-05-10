c8dadbd1366724ccd69324a215d1313407928f51
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-05-10 22:08:21 +0200

    Ajout de la fonction setActiveFilterButton

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -50,23 +50,42 @@ function displayFilteredProjects(categoryName) {
   });
 }
 
+function setActiveFilterButton(clickedButton) {
+  // Boucle dans un array avec tous les btn de filtre
+  [allBtn, objetsBtn, appartementsBtn, hotelsRestaurantsBtn].forEach(
+    (button) => {
+      // Applique le style par défaut
+      button.style.color = "#1d6154";
+      button.style.backgroundColor = "white";
+    }
+  );
+  // Applique le style du bouton de filtre actif / cliqué
+  clickedButton.style.color = "white";
+  clickedButton.style.backgroundColor = "#1d6154";
+}
+
 function filterProjects() {
   allBtn.addEventListener("click", () => {
+    setActiveFilterButton(allBtn);
     displayAllProjects();
   });
 
   objetsBtn.addEventListener("click", () => {
+    setActiveFilterButton(objetsBtn);
     displayFilteredProjects("Objets");
   });
 
   appartementsBtn.addEventListener("click", () => {
+    setActiveFilterButton(appartementsBtn);
     displayFilteredProjects("Appartements");
   });
 
   hotelsRestaurantsBtn.addEventListener("click", () => {
+    setActiveFilterButton(hotelsRestaurantsBtn);
     displayFilteredProjects("Hotels & restaurants");
   });
 
+  setActiveFilterButton(allBtn); // On active le bouton "Tous" par défaut
   displayAllProjects(); // Affiche tous les projets par défaut
 }
 
