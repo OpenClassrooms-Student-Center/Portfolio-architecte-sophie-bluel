d81bdf76df01e674cb91ea368744195da20b0a99
EvivNotrub <barthwerle@yahoo.de>
2023-05-19 12:08:54 +0200

    stash changes from branch filters

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -27,7 +27,8 @@ let works = window.localStorage.getItem("works");
 if (works == null){
     const worksResponse = await fetch('http://localhost:5678/api/works');
     works = await worksResponse.json();
-    window.localStorage.setItem("works", JSON.stringify(works));
+    window.localStorage.setItem("works", JSON.stringify(works)); // attention pas besoin ni de creer 
+    // une nouvelle variable ni meme d'utiliser le local storage
  }else{
     works = JSON.parse(works);
 }
@@ -42,29 +43,30 @@ function createProjectContent() {
         const work = document.createElement("figure");
         const workImage = document.createElement("img")
         const workTitle = document.createElement("figcaption");
-
+        work.classList.add(`cat${works[i].category.id}`);
         workImage.src = works[i].imageUrl;
         workTitle.textContent = works[i].title;
 
         gallery.appendChild(work);
         work.appendChild(workImage);
         work.appendChild(workTitle);
-
+console.log(works);
     }
 };
 function createCategoriesFilters() {
     const categoriesList = ["Tous"];
-
+    const categoriesId = [];
     
     for (let i in works) {
         if (categoriesList.includes(works[i].category.name) == false) {
             categoriesList.push(works[i].category.name);
+            categoriesId.push(works[i].category.id);
+            console.log(categoriesList);
+            console.log(categoriesId);
         }
     }
-
+    
     const filters = document.querySelector(".filters");
-    console.log(filters);
-
     for (let i in categoriesList) {
         const filter = document.createElement("div");
         filter.classList.add("filter");
@@ -81,7 +83,23 @@ function createCategoriesFilters() {
         filter.appendChild(checkBox);
         filter.appendChild(button);
     }
- }
+    
+}
+
+// I want to create a function that will filter the projects by category when checking the boxes
+function filterProjects() {
+    const filterChecks = document.querySelectorAll(".filterCheck");
+    const gallery = document.querySelector(".gallery");
+    console.log(filterButtons);
+    console.log(filterChecks);
+    console.log(gallery);
+
+    
+}
+
+
+
+
 
 function main (){
     createProjectContent();
