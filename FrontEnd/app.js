da81aa527e63b76d54c5773387191efe2c7c9542
marie-bcc <127097512+marie-bcc@users.noreply.github.com>
2023-06-23 15:17:08 +0200

    Ajout de la fenêtre modale

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,20 +1,47 @@
-// Récupère les données de l'API
 function fetchWorks() {
-    return fetch('http://localhost:5678/api/works')
-        .then(response => response.json())
-        .catch(error => console.error('Erreur:', error));
+    const token = localStorage.getItem('token');
+
+    // Si le token n'existe pas, redirection vers la page de connexion
+    if (!token) {
+        if (window.location.pathname !== '/FrontEnd/login.html') {
+            window.location.href = '/FrontEnd/login.html';
+        }
+        return Promise.reject('No token');
+    }
+
+    return fetch('http://localhost:5678/api/works', {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+
+            // Si le token n'est pas valide
+            if (error.message == '401') {
+                localStorage.removeItem('token');
+                window.location.href = '/FrontEnd/login.html';
+            }
+        });
 }
 
 // Organise les données par catégorie
 function organizeByCategory(data) {
-    return data.reduce((acc, item) => {
+    const result = {};
+    for (let item of data) {
         const categoryName = item.category.name;
-        if (!acc[categoryName]) {
-            acc[categoryName] = [];
+        if (!result[categoryName]) {
+            result[categoryName] = [];
         }
-        acc[categoryName].push(item);
-        return acc;
-    }, {});
+        result[categoryName].push(item);
+    }
+    return result;
 }
 
 // Initialise l'élément de la galerie
@@ -24,6 +51,10 @@ function setupGalleryElement() {
     element.style.gridTemplateColumns = 'repeat(3, 1fr)';
     element.style.gap = '10px';
     return element;
+}
+
+function appendModal() {
+
 }
 
 // Ajoute un élément de travail à la galerie
@@ -36,8 +67,6 @@ function appendWorkToGallery(work, galleryElement) {
 
     // crée un nouvel élément img
     let img = document.createElement('img');
-    img.style.width = '100%';
-    img.style.height = '413px';
     img.style.margin = '10px';
     let title = document.createElement('h3');
 
@@ -128,20 +157,97 @@ function appendElementsToContent(filterContainerElement, galleryElement) {
 
 // Utilisation des fonctions 
 fetchWorks().then(data => {
-    const categories = organizeByCategory(data);
-    const galleryElement = setupGalleryElement();
-    const filterContainerElement = createFilterContainer();
-    addCategoryButtonsToFilterContainer(categories, data, filterContainerElement);
-    appendElementsToContent(filterContainerElement, galleryElement);
+    if (data) {
+        const categories = organizeByCategory(data);
+        const galleryElement = setupGalleryElement();
+        const filterContainerElement = createFilterContainer();
+        addCategoryButtonsToFilterContainer(categories, data, filterContainerElement);
+        appendElementsToContent(filterContainerElement, galleryElement);
+        filterContainerElement.querySelector('button').click();
+    }
+});
 
-    // Déclenche manuellement l'événement 'click' sur le premier bouton pour remplir la galerie avec tous les travaux
-    filterContainerElement.querySelector('button').click();
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    })
+        .then(response => {
+            if (response.status == "200") {
+                return response.json();
+            } else {
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                window.location.href = '/FrontEnd/Homepage_edit.html';
+                return
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const errorMessageElement = document.getElementById('errorMessage');
+            errorMessageElement.textContent = 'Erreur dans l’identifiant ou le mot de passe';
+            errorMessageElement.classList.add('error');
+            errorMessageElement.style.display = 'block';
+        });
 });
 
 
+let modal = null
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display = null
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-close-modal').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
 
 
+}
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = 'none'
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-close-modal').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal = null
+}
 
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+
+})
+
+const modalGallery = document.querySelector('#modal1 .gallerie');
+
+fetchWorks().then(data => {
+    // Ajoute les travaux à la galerie modale
+    const modalGalleryElement = document.querySelector('#modal1 .gallerie');
+    data.forEach(work => appendWorkToGallery(work, modalGalleryElement));
+
+});
 
 
 
