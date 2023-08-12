2ca4d88819b56c2c809b3689a172f4fb9a7e17f9
rezozero59 <laurentdelassus@gmail.com>
2023-08-12 14:32:44 +0200

    suite

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,15 +1,24 @@
-fetch('http://localhost:5678/api/works')
-    .then(response => {
-        // Vérifiez si la requête a été réussie
-        if (!response.ok) {
-            throw new Error('Problème avec la requête');
-        }
-        return response.json(); // retourne les données en format JSON
-    })
-    .then(data => {
-        console.log(data); // Affiche les données dans la console
-    })
-    .catch(error => {
-        console.error('Erreur:', error); // Gère les erreurs éventuelles
-    });
+// let projet = fetch('http://localhost:5678/api/works')
+//     .then(response => {
+//         // Vérifiez si la requête a été réussie
+//         if (!response.ok) {
+//             throw new Error('Problème avec la requête');
+//         }
+//         return response.json(); // retourne les données en format JSON
+//     })
+//     .then(data => {
+//         alert(data); // Affiche les données dans la console
+//     })
+   
+//     .catch(error => {
+//         console.error('Erreur:', error); // Gère les erreurs éventuelles
+//     });
+
+
+    
+
+
+
+
+
     
