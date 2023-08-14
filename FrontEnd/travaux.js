062bdc2667a987fa72b78fb6033915b737927444
rezozero59 <laurentdelassus@gmail.com>
2023-08-14 22:37:26 +0200

    Works: Data ok, create element ok, append chil ok

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,24 +1,38 @@
-// let projet = fetch('http://localhost:5678/api/works')
-//     .then(response => {
-//         // Vérifiez si la requête a été réussie
-//         if (!response.ok) {
-//             throw new Error('Problème avec la requête');
-//         }
-//         return response.json(); // retourne les données en format JSON
-//     })
-//     .then(data => {
-//         alert(data); // Affiche les données dans la console
-//     })
-   
-//     .catch(error => {
-//         console.error('Erreur:', error); // Gère les erreurs éventuelles
-//     });
+async function fetchWorks() {
+  try {
+    let response = await fetch("http://localhost:5678/api/works");
+    if (response.ok === true) {
+      return response.json();
+    }
+    throw new Error("Problème avec la requête");
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+// let worksData = fetchWorks().then((wor) => console.log(wor));
 
-    
+let worksData = fetchWorks().then((data) => {
+  worksData = data;
+  // Ici, vous pouvez utiliser worksData comme vous le souhaitez
+  for (let i = 0; i < worksData.length; i++) {
+    // Variable projet qui contient le tableau d'objets du projet
+    let projet = worksData[i];
+    console.log(projet);
+    // Création de la div qui contiendra le projet
+    const sectionProjet = document.querySelector(".projets");
+    // Création des balises enfants de la div
+    const projetDivChild = document.createElement("figure");
 
+    const projetDivChildImg = document.createElement("img");
+    projetDivChildImg.src = projet.imageUrl;
+    const projetDivChildFigcaption = document.createElement("figcaption");
+    projetDivChildFigcaption.innerText = projet.title;
 
+    sectionProjet.appendChild(projetDivChild);
+    projetDivChild.appendChild(projetDivChildImg);
+    projetDivChild.appendChild(projetDivChildFigcaption);
+  }
 
-
-
-    
+  // alert(`bonjour ${worksData[0].title}`);
+});
