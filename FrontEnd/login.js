14893cf106bc605648ffea587f05c1ea04718e33
rezozero59 <laurentdelassus@gmail.com>
2023-08-20 10:15:38 +0200

    Suite de la modification du fichier login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#login form");
-  //   console.log(form);
+  const form = document.querySelector("#login");
+  // console.log(form); OK
   form.addEventListener("submit", async function (event) {
     event.preventDefault();
+    console.log("submit");
 
     const email = document.querySelector("#login-email").value;
-    // console.log(email);
+    console.log(email);
     const password = document.querySelector("#login-password").value;
-    // console.log(password);
+    console.log(password);
 
     try {
       const response = await fetch("http://localhost:5678/api/users/login", {
@@ -22,20 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 
       const data = await response.json();
+      console.log(data);
 
       if (response.status === 200) {
         // Store user data and/or token for future requests or to determine user state
-        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("user", JSON.stringify(data.userId));
         localStorage.setItem("token", data.token);
 
         // Redirect to the homepage or show additional options
-        location.href = "index.html";
+        // location.href = "index.html";
       } else {
         // Handle errors, e.g., show a message to the user
         alert(data.message);
       }
     } catch (error) {
-      console.error("There was an error during the login process:", error);
+      console.error("Une erreur est survenue", error);
     }
   });
   //   Chargement logout
