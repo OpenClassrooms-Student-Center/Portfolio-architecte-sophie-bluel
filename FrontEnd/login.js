a4bc296e1da62e7d043795c69880932a2e36dcc7
rezozero59 <laurentdelassus@gmail.com>
2023-08-20 19:02:16 +0200

    TEST refactorisation des fonctions de login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,62 +1,129 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#login");
-  // console.log(form); OK
-  form.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    console.log("submit");
+// --------------VERSION 1-----------------
 
-    const email = document.querySelector("#login-email").value;
-    console.log(email);
-    const password = document.querySelector("#login-password").value;
-    console.log(password);
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.querySelector("#login");
+//   // console.log(form); OK
+//   form.addEventListener("submit", async function (event) {
+//     event.preventDefault();
+//     console.log("submit");
 
-    try {
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
+//     const email = document.querySelector("#login-email").value;
+//     console.log(email);
+//     const password = document.querySelector("#login-password").value;
+//     console.log(password);
 
-      const data = await response.json();
-      console.log(data);
+//     try {
+//       const response = await fetch("http://localhost:5678/api/users/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           email: email,
+//           password: password,
+//         }),
+//       });
 
-      if (response.status === 200) {
-        // Store user data and/or token for future requests or to determine user state
-        localStorage.setItem("user", JSON.stringify(data.userId));
-        localStorage.setItem("token", data.token);
+//       const data = await response.json();
+//       console.log(data);
 
-        // Redirect to the homepage or show additional options
-        location.href = "index.html";
-      } else {
-        // Handle errors, e.g., show a message to the user
-        const messageError = "Identifiant ou mot de passe incorrect";
-        alert(messageError);
-        console.log(messageError);
-      }
-    } catch (error) {
-      console.error("Une erreur est survenue", error);
-    }
+//       if (response.status === 200) {
+//         // Store user data and/or token for future requests or to determine user state
+//         localStorage.setItem("user", JSON.stringify(data.userId));
+//         localStorage.setItem("token", data.token);
+
+//         // Redirect to the homepage or show additional options
+//         location.href = "index.html";
+//       } else {
+//         // Handle errors, e.g., show a message to the user
+//         const messageError = "Identifiant ou mot de passe incorrect";
+//         alert(messageError);
+//         console.log(messageError);
+//       }
+//     } catch (error) {
+//       console.error("Une erreur est survenue", error);
+//     }
+//   });
+//   //   Profil utilisateur connecté: liens du lien login --> logout
+//   function checkTokenLogin() {
+//     let tokenAuth = localStorage.getItem("token");
+//     // console.log(tokenAuth);ok
+//     let loginLink = document.getElementById("login-link");
+//     // console.log(loginLink); ok
+//     if (tokenAuth) {
+//       // Si authToken existe, l'utilisateur est probablement connecté
+//       loginLink.textContent = "Logout";
+//       loginLink.setAttribute("href", "logout.html");
+//     } else {
+//       // Sinon, réinitialisez-le pour s'assurer qu'il indique "Login" et pointe vers la page de connexion
+//       loginLink.textContent = "Login";
+//       loginLink.setAttribute("href", "login.html");
+//     }
+//   }
+//   checkTokenLogin();
+// });
+
+// -------VERSION 2--------TEST REFACTORISATION
+
+function initializeFormListener() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#login");
+    form.addEventListener("submit", submitForm);
   });
-  //   Profil utilisateur connecté: liens du lien login --> logout
-  function checkTokenLogin() {
-    let tokenAuth = localStorage.getItem("token");
-    // console.log(tokenAuth);ok
-    let loginLink = document.getElementById("login-link");
-    // console.log(loginLink); ok
-    if (tokenAuth) {
-      // Si authToken existe, l'utilisateur est probablement connecté
-      loginLink.textContent = "Logout";
-      loginLink.setAttribute("href", "logout.html");
-    } else {
-      // Sinon, réinitialisez-le pour s'assurer qu'il indique "Login" et pointe vers la page de connexion
-      loginLink.textContent = "Login";
-      loginLink.setAttribute("href", "login.html");
-    }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  console.log("submit");
+
+  const { email, password } = getFormData();
+
+  handleLogin(email, password);
+}
+
+function getFormData() {
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
+
+  console.log(email);
+  console.log(password);
+
+  return { email, password };
+}
+
+async function handleLogin(email, password) {
+  try {
+    const response = await loginRequest(email, password);
+    handleResponse(response);
+  } catch (error) {
+    console.error("Une erreur est survenue", error);
   }
-  checkTokenLogin();
-});
+}
+
+async function loginRequest(email, password) {
+  return await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
+}
+
+async function handleResponse(response) {
+  const data = await response.json();
+  console.log(data);
+
+  if (response.status === 200) {
+    localStorage.setItem("user", JSON.stringify(data.userId));
+    localStorage.setItem("token", data.token);
+    location.href = "index.html";
+  } else {
+    const messageError = "Identifiant ou mot de passe incorrect";
+    alert(messageError);
+    console.log(messageError);
+  }
+}
