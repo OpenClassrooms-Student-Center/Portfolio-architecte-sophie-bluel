22dbce5a6156b493c7b22b5f7efdb563e4bd2a06
Julie Reynal <rjulie.pro@gmail.com>
2023-08-24 17:56:37 +0200

    (Works): step 1.2 - filters on works

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -5,7 +5,7 @@ const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 // console.log(works);
 
-// Génération des articles
+// Génération des articles (avec createElement)
 function generateWorks(works) {
   for (let i = 0; i < works.length; i++) {
     const figure = works[i];
@@ -30,30 +30,82 @@ function generateWorks(works) {
 // generateWorks(works);
 
 
-// Génération des articles avec Template literals
+// Génération des articles (avec Template literals)
 function generateWorksWithTemplateLiterals(works) {
   for (let i = 0; i < works.length; i++) {
     const figure = works[i];
     // Récupération de l'élément du DOM qui accueillera les fiches
     const sectionGallery = document.querySelector('.gallery');
-
     // Création d’une balise dédiée à un work
     const workElement = document.createElement("figure");
     workElement.dataset.id = works[i].id;
-
     // Création des variables à indenter imagesUrl et title
     const imageUrl = figure.imageUrl;
     const title = figure.title ?? "(aucun titre)";
-
     // Ajout des variables et du Html associé à workElement
     workElement.innerHTML=
       `<img src="${imageUrl}" alt="${title}">
       <figcaption> ${title}</figcaption>`
     ;
-
     // On rattache la balise figure a la section Gallery
     sectionGallery.appendChild(workElement);
   }
 }
 
 generateWorksWithTemplateLiterals(works);
+
+
+///////////// FILTERS //////////////
+
+
+// Récupération des categories depuis l'API
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponseCategories.json();
+
+// Création de l'ensemble des filtres
+let filters = []
+filters.push("Tous")
+categories.forEach(element => {
+  filters.push(element.name)
+});
+console.log(filters);
+
+// Génération des filtres
+function generateFilters(filters) {
+  for (let i = 0; i < filters.length; i++) {
+    const categorie = filters[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFilters = document.querySelector('.filters');
+    // Création d’une div dédiée à une catégorie
+    const filterCategory = document.createElement("div");
+    filterCategory.dataset.id = filters[i].id;
+    // Ajout du Html associé à workElement
+    filterCategory.innerHTML = categorie;
+    // Ajout du css
+    filterCategory.classList.add("filter");
+    // On rattache la balise figure a la section Gallery
+    sectionFilters.appendChild(filterCategory);
+  }
+}
+generateFilters(filters);
+
+
+// Bouton Filtrer pour chaque categorie
+const buttonCategories = document.querySelectorAll(".filter");
+
+buttonCategories.forEach(buttonCategory => {
+  if (buttonCategory.innerText === "Tous") {
+    buttonCategory.addEventListener("click", function () {
+      document.querySelector(".gallery").innerHTML = "";
+      generateWorksWithTemplateLiterals(works);
+    });
+  } else {
+    buttonCategory.addEventListener("click", function () {
+      const FilteredWorks = works.filter(function (work) {
+        return work.category.name === buttonCategory.innerText;
+      });
+      document.querySelector(".gallery").innerHTML = "";
+      generateWorksWithTemplateLiterals(FilteredWorks);
+    });
+  }
+});
