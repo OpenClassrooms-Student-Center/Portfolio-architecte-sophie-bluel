80623fd561e3b2ad2918497df5f74b06e14d5e5d
rezozero59 <laurentdelassus@gmail.com>
2023-08-28 16:19:29 +0200

    Login2.JS Factory Functions, module mains.js

--- FrontEnd/main2.js
+++ FrontEnd/main2.js
@@ -1,11 +1,10 @@
 import { initWorks } from "./works2.js";
+import { initLoginForm, checkTokenLogin } from "./login2.js";
+
+// Vérifier l'état de connexion de l'utilisateur
+checkTokenLogin();
 
 // Lancer l'initialisation des travaux
 initWorks();
 
-// Ajout de l'écouteur d'événements pour la soumission du formulaire
-const form = document.getElementById("login");
-form.addEventListener("submit", handleFormSubmission);
-
-// Vérification de l'état de connexion à l'initialisation
-checkTokenLogin();
+initLoginForm();
