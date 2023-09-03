4e82958e732b6d068758e8d486a2581ed650e4ff
rezozero59 <laurentdelassus@gmail.com>
2023-09-03 20:21:39 +0200

    Bug filtres en mode connecté

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -54,4 +54,59 @@ document
       toggleModal(false);
     }
   });
-// -------------------------------------------------------------------------------------------------
+
+// ---------------------Formulaire envoi photo---------------------------------------------------
+const addPhotoBtn = document.getElementById("add-photo");
+
+addPhotoBtn.addEventListener("click", function () {
+  // Cachez la galerie d'images et affichez le formulaire
+
+  document.getElementById("edit-modal").classList.add("hidden");
+  document
+    .getElementById("add-photo-form-container")
+    .classList.remove("hidden");
+});
+
+const addPhotoForm = document.getElementById("add-photo-form");
+// console.log(addPhotoForm);
+
+if (addPhotoForm)
+  addPhotoForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const imageUpload = document.getElementById("image-upload").files[0];
+    const projectTitle = document.getElementById("project-title").value;
+    const projectCategory = document.getElementById("project-category").value;
+
+    // Validation
+    if (!imageUpload || !projectTitle || !projectCategory) {
+      document.getElementById("form-error-message").innerText =
+        "Veuillez remplir tous les champs.";
+      return;
+    }
+
+    // Création de l'objet FormData pour envoyer le fichier et les autres données
+    const formData = new FormData();
+    formData.append("image", imageUpload);
+    formData.append("title", projectTitle);
+    formData.append("categoryId", projectCategory);
+
+    // Envoi à l'API
+    const token = localStorage.getItem("token");
+    const response = await fetch("http://localhost:5678/api/works", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (response.ok) {
+      // Réponse de l'API si le formulaire est correctement envoyé
+      alert("Projet ajouté avec succès!");
+      location.reload(); // Recharger la page pour voir le nouveau projet
+    } else {
+      // Message d'erreur
+      alert("Une erreur s'est produite. Veuillez réessayer.");
+    }
+  });
