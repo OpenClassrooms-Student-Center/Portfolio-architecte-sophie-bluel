74296e0a5ecdb8e1b0db3df6a3acc657ff641cf0
morganhel <138800018+morganhel@users.noreply.github.com>
2023-09-13 17:49:05 +0200

    Affichage des projets avec API

--- FrontEnd/scripts/gallery.js
+++ FrontEnd/scripts/gallery.js
@@ -1,6 +1,32 @@
-//Récupération des travaux
+//Récupération et affichage des travaux
 
-// const reponse = fetch("http://localhost:5678/works");
-// const works= reponse.json()
+async function AfficherTravaux() {
+    const reponse = await fetch('http://localhost:5678/api/works');
+    const works = await reponse.json();
 
-// console.log(works)
+    document.querySelector(".gallery").innerHTML="";
+    
+    for (let i=0; i<works.length; i++){
+        const projet = works[i];
+
+        //Recup elt du DOM qui accueillera les projets
+        const gallery = document.querySelector(".gallery");
+
+        // Création d’une balise dédiée à un projet
+        const figure = document.createElement("figure");
+
+        // Création des balises 
+        const imageProjet = document.createElement("img");
+        imageProjet.src=projet.imageUrl;
+        const titreProjet = document.createElement("figcaption");
+        titreProjet.innerHTML=projet.title;
+
+        // Ajout nouveaux elt au DOM
+        await gallery.appendChild(figure);
+        await figure.appendChild(imageProjet);
+        await figure.appendChild(titreProjet);
+    }
+};
+
+AfficherTravaux();
+
