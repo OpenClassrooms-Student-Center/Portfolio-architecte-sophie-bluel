84e53ce5e92cd3e8f4e7a59cd7a3662d5130d8f9
rezozero59 <laurentdelassus@gmail.com>
2023-09-19 10:41:40 +0200

    Réglages css modale ajout de photos

---
+++ FrontEnd/testserver.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static("assets"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views/index.html"));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening at http://localhost:${port}`);
+// });
