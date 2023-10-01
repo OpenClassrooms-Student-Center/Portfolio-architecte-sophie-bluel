bc3d56a5d96f846e52917be659edcd14a1a77519
Julie Reynal <rjulie.pro@gmail.com>
2023-10-01 14:58:02 +0200

    modal index fetch icons

--- FrontEnd/scripts/edit.js
+++ FrontEnd/scripts/edit.js
@@ -1,5 +1,40 @@
 ///////////// MODAL //////////////
 
+// Récupération des works depuis l'API
+const reponse = await fetch("http://localhost:5678/api/works");
+const works = await reponse.json();
+console.log(works);
+
+function generateIndex(works) {
+  for (let i = 0; i < works.length; i++) {
+    const icon = works[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionGalleryModal = document.querySelector(".icons-gallery");
+    // Création d’une balise dédiée à un work
+    const workElement = document.createElement("div");
+    workElement.dataset.id = works[i].id;
+    workElement.classList.add("icon-modal");
+    // Création de la balise img
+    const imageElement = document.createElement("img");
+    imageElement.src = icon.imageUrl;
+    // Création de l'icône Delete
+    const trashElement = document.createElement("button");
+    console.log(trashElement);
+
+    trashElement.dataset.id = works[i].id;
+    trashElement.classList.add("js-delete-btn");
+    trashElement.classList.add("delete-btn");
+    trashElement.setAttribute("id", `js-delete-btn-${works[i].id}`);
+    trashElement.innerHTML=`<i class="fas fa-trash-alt"></i>`;
+    console.log(trashElement);
+
+    // On rattache la balise icon a la section Gallery
+    sectionGalleryModal.appendChild(workElement);
+    workElement.appendChild(imageElement);
+    workElement.appendChild(trashElement);
+  }
+}
+
 function generateModal() {
   const modal = document.querySelector('#modal');
 
@@ -19,6 +54,7 @@ function generateModal() {
   })
 
   // Générer modal index
+  generateIndex(works);
 
   // Générer modal new
 
@@ -32,6 +68,8 @@ generateModal();
 // interaction via les modal
 // attendu ne doit pas recharger la page
 // toutes les modifs doivent se faire avec le dom, et uniquement la partie concernée par la modification.
+
+
 
 
 ///////// MODIFY BUTTON ///////////////////
