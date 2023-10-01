adfd52fbec4d0ee3ba944d119beca996e05786c3
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2023-10-01 18:27:53 +0200

    Importation des travaux par le fichier galerie.js

---
+++ FrontEnd/galerie.js
@@ -0,0 +1,64 @@
+// Construction de la fonction appel API
+async function appelTravaux() {
+
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const works = await reponse.json();
+
+    // Appel de la fonction pour générer les travaux après avoir obtenu les données
+    genererTravaux(works);
+ };
+
+function genererTravaux(works) {
+  // Récupération de l'élément du DOM qui accueillera les travaux
+  const galleryList = document.querySelector(".gallery");
+
+  for (let i = 0; i < works.length; i++) {
+    const travail = works[i];
+
+    // Création d'une balise dédiée à un travail
+    const figureElement = document.createElement("figure");
+
+    // Création de la balise image
+    const imageElement = document.createElement("img");
+    imageElement.src = travail.imageUrl;
+
+    // Création de la balise figcaption
+    const figCaptionElement = document.createElement("figcaption");
+    figCaptionElement.innerText = travail.title;
+
+    // On rattache les éléments enfants aux parents
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(figCaptionElement);
+
+    // On rattache figure à son parent
+    galleryList.appendChild(figureElement);
+  }
+};
+
+// Appel de la fonction pour récupérer les données
+appelTravaux();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
