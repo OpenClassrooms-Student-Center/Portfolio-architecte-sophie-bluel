f728d9f655d411df1ad0dbf42d22b3c6b601bc84
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2023-10-06 15:42:19 +0200

    Intégration des boutons filtres par catégorie

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -1,16 +1,19 @@
+let works = [];
+const galleryList = document.querySelector(".gallery");
 // Construction de la fonction appel API
 async function appelTravaux() {
 
     const reponse = await fetch("http://localhost:5678/api/works");
-    const works = await reponse.json();
-
-    // Appel de la fonction pour générer les travaux après avoir obtenu les données
-    genererTravaux(works);
+    works = await reponse.json();
+    
+// Appel de la fonction pour générer les travaux après avoir obtenu les données
+genererTravaux(works);
+    
  };
 
 function genererTravaux(works) {
   // Récupération de l'élément du DOM qui accueillera les travaux
-  const galleryList = document.querySelector(".gallery");
+  //const galleryList = document.querySelector(".gallery"); **élément affiché en haut de page**
 
   for (let i = 0; i < works.length; i++) {
     const travail = works[i];
@@ -33,32 +36,63 @@ function genererTravaux(works) {
     // On rattache figure à son parent
     galleryList.appendChild(figureElement);
   }
+  
 };
 
 // Appel de la fonction pour récupérer les données
 appelTravaux();
 
+//Récupération des boutons dans le DOM
+const globalButton = document.querySelector(".button.global");
+const objetsButton = document.querySelector(".button.objets");
+const appartementsButton = document.querySelector(".button.appartements");
+const hotelsRestaurantsButton = document.querySelector(".button.hotels");
+
+// Ajout d'un gestionnaire d'événements à chaque bouton
 
 
+//Bouton afficher global
+globalButton.addEventListener("click",function(event){
+  event.preventDefault();
+const globalFilter = works.filter((work) => work.categoryId===work.categoryId);
+//Initialisation de l'affichage de ma galerie
+galleryList.innerHTML = "";
+//Affichage des valeurs de mon filtre objets
+genererTravaux(globalFilter);
+});
 
 
+//Bouton afficher objets
+objetsButton.addEventListener("click",function(event){
+  event.preventDefault();
+  //Création de mon contenu de filtre pour objet
+  const objetsFiltres = works.filter((work) => work.categoryId===1);
+ //Initialisation de l'affichage de ma galerie
+  galleryList.innerHTML = "";
+  //Affichage des valeurs de mon filtre objets
+  genererTravaux(objetsFiltres);
+  });
 
 
+//Bouton afficher appartements
+appartementsButton.addEventListener("click",function(event){
+  event.preventDefault();
+  //Création de mon contenu de filtre pour appartements
+  const appartementsFiltres = works.filter((work) => work.categoryId===2);
+ //Initialisation de l'affichage de ma galerie
+  galleryList.innerHTML = "";
+  //Affichage des valeurs de mon filtre appartements
+  genererTravaux(appartementsFiltres);
+  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
+  //Bouton afficher hôtels restaurants
+  hotelsRestaurantsButton.addEventListener("click",function(event){
+    event.preventDefault();
+    //Création de mon contenu de filtre pour hôtels restaurants
+    const hotelsRestaurantsFiltres = works.filter((work) => work.categoryId===3);
+   //Initialisation de l'affichage de ma galerie
+    galleryList.innerHTML = "";
+    //Affichage des valeurs de mon filtre hôtels restaurants
+    genererTravaux(hotelsRestaurantsFiltres);
+    });
