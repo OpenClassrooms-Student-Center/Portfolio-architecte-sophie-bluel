c94f791767db5f85b8c5642931abf20d856db941
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2023-10-10 11:37:31 +0200

    >Plus d'erreurs sur la page projet, erreurs sur login / division des fichiers JS

--- FrontEnd/filtrage.js
+++ FrontEnd/filtrage.js
@@ -1,48 +1,4 @@
-let works = [];
-const galleryList = document.querySelector(".gallery");
-// Construction de la fonction appel API
-async function appelTravaux() {
-
-    const reponse = await fetch("http://localhost:5678/api/works");
-    works = await reponse.json();
-    
-// Appel de la fonction pour générer les travaux après avoir obtenu les données
-genererTravaux(works);
-    
- };
-
-function genererTravaux(works) {
-  // Récupération de l'élément du DOM qui accueillera les travaux
-  //const galleryList = document.querySelector(".gallery"); **élément affiché en haut de page**
-
-  for (let i = 0; i < works.length; i++) {
-    const travail = works[i];
-
-    // Création d'une balise dédiée à un travail
-    const figureElement = document.createElement("figure");
-
-    // Création de la balise image
-    const imageElement = document.createElement("img");
-    imageElement.src = travail.imageUrl;
-
-    // Création de la balise figcaption
-    const figCaptionElement = document.createElement("figcaption");
-    figCaptionElement.innerText = travail.title;
-
-    // On rattache les éléments enfants aux parents
-    figureElement.appendChild(imageElement);
-    figureElement.appendChild(figCaptionElement);
-
-    // On rattache figure à son parent
-    galleryList.appendChild(figureElement);
-  }
-  
-};
-
-// Appel de la fonction pour récupérer les données
-appelTravaux();
-
-/*Récupération des boutons dans le DOM
+//Récupération des boutons dans le DOM
 const globalButton = document.querySelector("#btnGlobal");
 const objetsButton = document.querySelector("#btnObjets");
 const appartementsButton = document.querySelector("#btnAppartements");
@@ -54,6 +10,7 @@ const hotelsRestaurantsButton = document.querySelector("#btnHotels");
 globalButton.addEventListener("click",function(event){
  
   event.preventDefault();
+  
   // Afficher tous les travaux (aucun filtrage)
   galleryList.innerHTML = "";
   genererTravaux(works);
@@ -92,4 +49,4 @@ appartementsButton.addEventListener("click",function(event){
     galleryList.innerHTML = "";
     //Affichage des valeurs de mon filtre hôtels restaurants
     genererTravaux(hotelsRestaurantsFiltres);
-    });*/
+    });
