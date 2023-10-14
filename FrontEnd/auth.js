d5936e16159c30b64ddbc96976b2609c06ea25a8
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2023-10-14 11:32:03 +0200

    Sauvegarde configuration login et mise à jour code

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -1,55 +1,42 @@
-
-//Garanti le bon chargement de mon HTML avant que le JavaScript ne s'exécute
-//(Evite des erreurs, assure un comportement attendu du site web, améliore la
-//réactivité de la page pour les utilisateurs. Garanti le fonctionnement du 
-//site de manière fluide et sans problème.)
 document.addEventListener("DOMContentLoaded", function () {
-
-  // Récupération de mon élément html form
   const loginForm = document.querySelector("form");
 
-  loginForm.addEventListener("submit", async function (event) {
-    // Empêche le formulaire de se soumettre de manière classique
-    event.preventDefault(); 
-    // Récupération des valeurs de mes éléments input utile à l'identification
+  // Récupération de l'élément du bouton Login
+  
+ // const loginButton = document.getElementById('loginButton');
+
+  loginForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    // Définition des éléments de ma variable user
     const user = {
       email: email,
       password: password,
     };
 
-    // Récupération de la réponse de l'API suite à la requête POST de l'utilisateur
+    // Récupération des valeurs de l'API
     const response = await fetch("http://localhost:5678/api/users/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      // Conversion de notre body en JSON
       body: JSON.stringify(user)
     });
 
-    if (response.ok) {
-      // Connexion réussie, récupération du jeton et stockage
-      const data = await response.json();
-      if (data.token) {
-        // Un token a été trouvé dans la réponse
-        const token = data.token;
-        // Stockage du token dans le localStorage
-        localStorage.setItem('token', token);
-        // Redirection vers la page index.html en cas de succès
-        window.location.href = "index.html";
-        return alert("MON TOKEN" + token);
-      } else {
-        // La réponse ne contient pas de token
-        alert("La réponse de l'API ne contient pas de token.");
-      }
-    } else {
-      // Affichage d'un message d'erreur dans le navigateur en cas d'échec
-      return alert("Vos identifiants ne sont pas valides");
-    };
-  });
+  if (response.ok) {
+          const data = await response.json();
+          if (data.token) {
+              const token = data.token;
+              localStorage.setItem('token', token);
+              localStorage.setItem('isLoggedIn', true);
+              window.location.href = "index.html";
+              alert("L'utilisateur est connecté");
+                  
+          };
+  }else{
+      alert("Vos identifiants ne sont pas valides");
+      alert("L'utilisateur n'est pas connecté");
+      alert("raté");
+  };
 });
-
-
+});
---
+++ FrontEnd/bisindex.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Vérifie si l'utilisateur est connecté
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  
+    // Récupération de l'élément du bouton de connexion
+    const loginButton = document.getElementById('loginButton');
+    const btnFilter = document.querySelector(".btn")
+    const blackRectangle = document.getElementById("blackRectangle")
+    const modifyButton = document.getElementById("modifyButton")
+    const logoutButton = document.getElementById("logoutButton")
+
+       if (isLoggedIn) {
+        // L'utilisateur est connecté, détermine le comportement de la page
+        loginButton.style.display = "none";
+        btnFilter.style.display = "none"
+        blackRectangle.style.display = "flex";
+        modifyButton.style.display = "flex";
+        logoutButton.style.display = "block";
+
+        // Ajout d'un addEventListener sur le bouton "Logout"
+        logoutButton.addEventListener('click', function () {
+            // Réinitialise l'indicateur de connexion dans le stockage local
+            localStorage.setItem('isLoggedIn', false);
+            // Redirige l'utilisateur vers la page de connexion (ou ailleurs si nécessaire)
+            window.location.href = "login.html";
+        });
+      
+    } else {
+      // L'utilisateur n'est pas connecté, détermine le comportement de la page
+
+      loginButton.style.display = "block";
+      blackRectangle.style.display = "none";
+      modifyButton.style.display = "none";
+      btnFilter.style.display = "flex";
+    }
+  });
