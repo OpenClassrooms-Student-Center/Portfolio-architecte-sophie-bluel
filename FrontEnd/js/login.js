b989f3ce29e77cb4d083edb96f3e4f0d38f33567
David Andaman <david.andaman@gmail.com>
2023-11-01 18:00:35 +0100

    input du formulaire ok avec un console.log

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,12 +1,33 @@
 import { loginPortfolio } from "./export-login-api.js"
 
-// Récuperer les éléments du HTML
+/** Récuperer les éléments du HTML */
 const email = document.getElementById("email");
-const loginPassword = document.getElementById("login-password");
+const Password = document.getElementById("login-password");
 const btnSubmit = document.getElementById("btn-login");
 
 let emailInput = ""
 let passwordInput = ""
 
+console.log(email)
 
-console.log(emailInput)
+/** Connexion avec l'input de l'email */
+email.addEventListener("input", (connexion) => {
+    emailInput = connexion.target.value
+})
+
+/** Connexion avec l'input du mot de passe */
+password.addEventListener("input", (connexion) => {
+    passwordInput = connexion.target.value
+})
+
+/** Connexion avec le bouton du login form */
+btnSubmit. addEventListener("click", async (connexion) => {
+    connexion.preventDefault()
+    let user = {"email": emailInput, "password": password};
+    console.log(user)
+})
+
+/** Comparaison et action avec les données de l'API */
+
+
+
