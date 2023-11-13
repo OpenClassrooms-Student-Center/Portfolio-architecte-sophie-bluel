60a9989f8ff323a1324b121123011596c7edcf09
David Andaman <david.andaman@gmail.com>
2023-11-13 15:11:12 +0100

    etape 5 - 50%

--- FrontEnd/js/projets-editor.js
+++ FrontEnd/js/projets-editor.js
@@ -2,7 +2,29 @@ import { deleteApi, works, postApi, categories } from "./export-projets-api.js";
 import { displayGallery } from "./projets.js";
 
 
-/** MODAL GALERIE DE PROJETS Start */
+/**
+************************************************
+******** MODAL GALERIE DE PROJETS Start ********
+************************************************
+ */
+
+// Fonction pour créer et ajouter topStroke
+const addTopStroke = (shouldDisplayTopStroke) => {
+  let divTopStroke = document.querySelector(".display-mini-gallery");
+
+  // Supprimer topStroke s'il existe déjà
+  let existingTopStroke = divTopStroke.querySelector(".top-stroke");
+  if (existingTopStroke) {
+    existingTopStroke.remove();
+  }
+
+  if (shouldDisplayTopStroke) {
+    let topStroke = document.createElement("div");
+    topStroke.classList.add("top-stroke");
+    topStroke.textContent = "";
+    divTopStroke.appendChild(topStroke);
+  }
+};
 
 let contenuTitreGalerie = "Galerie Photo";
 let divTitreGalerie = document.createElement("div");
@@ -17,6 +39,7 @@ const miniGallery = document.querySelector(".modal-portfolio");
 
 export const miniDisplayGallery = (projet) => {
   document.querySelector(".modal-portfolio").innerHTML = "";
+
   for (let i in projet) {
     /** Création des balises */
     const displayMiniProject = document.createElement("display-mini-project");
@@ -39,65 +62,77 @@ export const miniDisplayGallery = (projet) => {
       }
     });
 
-    // Ajoutez l'ID du projet en tant qu'attribut data-id à l'élément display-mini-project
+    // Ajoutez l'ID du projet en tant qu'attribut data-id
+    // à l'élément display-mini-project.
     displayMiniProject.setAttribute("data-id", projectId);
-
     displayMiniProject.appendChild(trashIcon);
     displayMiniProject.appendChild(image);
 
     miniGallery.appendChild(displayMiniProject);
   }
+    addTopStroke(true);
 };
 
 miniDisplayGallery(works);
 
-// Footer du modal avec le trait et le bouton
-let topStroke = document.createElement("div");
-topStroke.classList.add("top-stroke");
-topStroke.textContent = "";
-
 let btnAjoutPhoto = document.createElement("button");
 btnAjoutPhoto.classList.add("submit-photo");
 btnAjoutPhoto.textContent = "Ajouter une photo";
-
-let divTopStroke = document.querySelector(".display-mini-gallery");
 let divBtnAjoutPhoto = document.querySelector(".display-mini-gallery");
-
-divTopStroke.appendChild(topStroke);
 divBtnAjoutPhoto.appendChild(btnAjoutPhoto);
 
-/** MODAL GALERIE DE PROJETS End */
 
 
-
-
-
-/** MODAL AJOUT PROJET Start */
+/**
+******************************************
+******** MODAL AJOUT PROJET Start ********
+******************************************
+ */
 
 const clearContent = () => {
-miniGallery.innerHTML = "";
+  miniGallery.innerHTML = "";
+};
+
+const hideBtnAjoutPhoto = () => {
+  btnAjoutPhoto.remove(); // Supprime le bouton du DOM
+};
+
+const showBtnAjoutPhoto = () => {
+  // Recréer le bouton btnAjoutPhoto
+  btnAjoutPhoto = document.createElement("button");
+  btnAjoutPhoto.classList.add("submit-photo");
+  btnAjoutPhoto.textContent = "Ajouter une photo";
+
+  // Ajouter le gestionnaire d'événements au bouton récréé
+  btnAjoutPhoto.addEventListener("click", generateAddImageForm);
+
+  // Ajouter le bouton au DOM
+  divBtnAjoutPhoto.appendChild(btnAjoutPhoto);
 };
 
 // Fonction pour générer le formulaire d'ajout d'image
-const generateAddImageForm = () => {
-// Appeler la fonction pour effacer le contenu HTML
-clearContent();
+const generateAddImageForm = (newImage) => {
+  // Appeler la fonction pour effacer le contenu HTML
+  clearContent();
+  // Cacher le bouton btnAjoutPhoto
+  hideBtnAjoutPhoto();
+
 
 // Créer le formulaire
   const addImageForm = document.createElement("form");
-
 // Ajouter un lien pour revenir à la galerie de projets
   const backButton = document.createElement("a");
   backButton.href = "#";
   backButton.textContent = "Revenir à la galerie de projets";
   backButton.addEventListener("click", () => {
     miniDisplayGallery(works);
+    showBtnAjoutPhoto();
   });
 
   backButton.classList.add("backButton");
   document.querySelector(".backform").appendChild(backButton);
 
-
+// Contenu du formulaire
 // Bouton pour ajouter une image
 let btnAjoutImage = document.createElement("button");
 btnAjoutImage.textContent = "Ajouter une image";
@@ -126,11 +161,16 @@ let submitAjoutPhotoBtn = document.createElement("button");
 submitAjoutPhotoBtn.textContent = "+ Ajout photo";
 submitAjoutPhotoBtn.type = "submit";
 
-  
+
+let topStrokeAddPhoto = document.createElement("div");
+topStrokeAddPhoto.classList.add("add-stroke");
+topStrokeAddPhoto.textContent = "";
+
 // Ajouter un bouton de soumission pour confirmer l'ajout de la nouvelle image
 const btnValiderNewProjet = document.createElement("button");
 btnValiderNewProjet.type = "submit";
 btnValiderNewProjet.textContent = "Valider";
+btnValiderNewProjet.classList.add("submit-photo");
 
 
   // Ajouter un gestionnaire d'événements pour le bouton de soumission
@@ -147,11 +187,20 @@ btnValiderNewProjet.textContent = "Valider";
   addImageForm.appendChild(categorieAjoutPhotoSelect);
   addImageForm.appendChild(submitAjoutPhotoBtn);
 
+  addImageForm.appendChild(topStrokeAddPhoto);
+
   addImageForm.appendChild(btnValiderNewProjet);
 
-  // Ajouter le formulaire à l'élément miniGallery
-  miniGallery.appendChild(addImageForm);
+
+
+// Ajouter le formulaire à l'élément miniGallery
+miniGallery.appendChild(addImageForm);
+
+addTopStroke(false);
+
 };
 
 // Ajouter un gestionnaire d'événement au bouton btnAjoutPhoto
+
 btnAjoutPhoto.addEventListener("click", generateAddImageForm);
+
