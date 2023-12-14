5c35c3ba49f058f2f23bf4fd0996ee32ef629b37
Marine Allavoine <allavoine.marine@gmail.com>
2023-12-14 17:20:19 +0100

    Etape 1.2 Ajout des filtres

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,13 +1,3 @@
-//Variables globales
-
-
-//fonctions
-
-
-
-
-
-
 
 
 // Scenario du projet: 
@@ -35,3 +25,55 @@
  //6. L utilisateur clique sur le bouton se deconnecter
     //6.a. L utilisateur est deconnecté
     //6.b. L utilisateur est redirigé vers la page d'accueil pour les utilisateurs non connectés
+
+
+    //TESTS
+
+    
+
+//variables globales
+
+
+
+//  const gallery = document.querySelector('.gallery')// selectionne la div "gallery"
+//const response = await fetch(`http://localhost:5678/api/works`)//recupere les donnees de l'api
+//console.log(response);//affiche les donnees de l'api
+//const projects = await response.json() //transforme les donnees en json
+
+//console.log(gallery);//affiche la classe gallery
+
+
+
+
+
+
+//1. L'utilisateur ajoute des images
+    //1.a. L utilisateur clique sur le bouton ajouter une image
+    //1.b. La fenetre d'ajout d'image s'ouvre
+    //1.c. L utilisateur selectionne une image
+    //1.d. L utilisateur doit nommer l'image
+    //1.d. L utilisateur clique sur le bouton ouvrir/ajouter
+    //1.e. La fenetre d'ajout d'image se ferme
+    //1.f. L'image se telecharge 
+    //1.g. L image est ajoutée à la liste des images de l utilisateur
+
+
+//2. L utilisateur supprime des images
+    //2.a. L utilisateur clique sur le bouton supprimer une image
+    //2.b. La fenetre de suppression d'image s'ouvre
+    //2.c. L utilisateur selectionne une image
+    //2.d. L utilisateur clique sur le bouton supprimer
+    //2.e. La fenetre de suppression d'image se ferme
+    //2.f. L'image est supprimée de la liste des images de l utilisateur
+
+
+//3. L'utilisateur filtre des images
+//3.a. L utilisateur clique sur le bouton filtrer
+//3.b. La fenetre de filtrage s'ouvre
+//3.c. L utilisateur selectionne un filtre
+//3.d. L utilisateur clique sur le bouton appliquer
+//3.e. La fenetre de filtrage se ferme
+//3.f. L'image est filtrée
+
+    
+
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,92 +1,89 @@
 //variables globales
 
+
+const figure = document.createElement("figure");//création de la balise figure
+const sectionGallery = document.querySelector(".gallery");//selectionne la div "gallery"
+const imageFigure = document.createElement("img");//création de la balise img
+const titleFigure = document.createElement("figcaption");//création de la balise figcaption
+const projectFigure = document.createElement("figure");//création de la balise figure
+const sectionPortfolio = document.getElementById("portfolio");//selectionne l'Id "portfolio"
+const filtersButtons = document.createElement("div");//création de la balise div
+
+const projects =  getGalleryProjects();
+
+
+
 //Fonctions 
-
-
-
+function main(){
+  getGalleryProjects();
+  displayProjects();
+  displayFiltersButtons()
+}
 
 
 //API FETCH
 
 async function getGalleryProjects(){
-    const reponse = await fetch("http://localhost:5678/api/works");
-    const projects = await reponse.json();
-console.log(projects);
-
-
-    for (let i = 0; i < projects.length; i++) {//boucle for pour parcourir les donnees de l'api
-      const figure = projects[i];//recupere les donnees de l'api
-     
-      const sectionGallery = document.querySelector(".gallery");//selectionne la div "gallery"
-
-      console.log(figure);//affiche les donnees de l'api
-
-      const projectFigure = document.createElement("figure");//création de la balise figure
-      projectFigure.dataset.id = projects[i].id;//ajout de l'attribut data-id
-      console.log(projectFigure);//affiche la balise figure
-
-      
-      const imageFigure = document.createElement("img");//création de la balise img
-      imageFigure.src = figure.imageUrl;//ajout de l'attribut src
-
-   
-      const titleFigure = document.createElement("figcaption");//création de la balise figcaption
-      titleFigure.innerText = figure.title ?? "(aucun titre)";//ajout du titre
-
-     
-      sectionGallery.appendChild(projectFigure);//ajout de la balise figure dans la div "gallery"
-      projectFigure.appendChild(imageFigure);//ajout de la balise img dans la balise figure
-      projectFigure.appendChild(titleFigure);//ajout de la balise figcaption dans la balise figure
-    }
+  const response = await fetch("http://localhost:5678/api/works");
+ return await response.json();
   }
+
+  getGalleryProjects();
+
+
+//Fonction pour afficher les projets de la galerie
+
+async function displayProjects(){
+  const projects = await getGalleryProjects();//recupere les donnees de l'api 
+  for (let i = 0; i < projects.length; i++) {//boucle for pour parcourir les donnees de l'api
+    const figure = projects[i];//recupere les donnees de l'api et les stocke dans la variable figure
+    const sectionGallery = document.querySelector(".gallery");//selectionne la div "gallery"
+ // console.log(figure);//affiche les donnees de l'api
   
-
-
-  getGalleryProjects ();//appel de la fonction getGalleryProjects
-
-// En cours de construction
-
-  async function getGalleryProjectsByName(name){
-    const reponse = await fetch("http://localhost:5678/api/works/" + name);
-    const projects = await reponse.json();
-console.log(projects);
-
-
-for (let i = 0; i < projects.length; i++) {
-const sectionPortfolio = document.getElementById("#portfolio");//selectionne l'Id "portfolio"
-
-
-  }
-  }
-
-//1. L'utilisateur ajoute des images
-    //1.a. L utilisateur clique sur le bouton ajouter une image
-    //1.b. La fenetre d'ajout d'image s'ouvre
-    //1.c. L utilisateur selectionne une image
-    //1.d. L utilisateur doit nommer l'image
-    //1.d. L utilisateur clique sur le bouton ouvrir/ajouter
-    //1.e. La fenetre d'ajout d'image se ferme
-    //1.f. L'image se telecharge 
-    //1.g. L image est ajoutée à la liste des images de l utilisateur
-
-
-//2. L utilisateur supprime des images
-    //2.a. L utilisateur clique sur le bouton supprimer une image
-    //2.b. La fenetre de suppression d'image s'ouvre
-    //2.c. L utilisateur selectionne une image
-    //2.d. L utilisateur clique sur le bouton supprimer
-    //2.e. La fenetre de suppression d'image se ferme
-    //2.f. L'image est supprimée de la liste des images de l utilisateur
-
-
-//3. L'utilisateur filtre des images
-//3.a. L utilisateur clique sur le bouton filtrer
-//3.b. La fenetre de filtrage s'ouvre
-//3.c. L utilisateur selectionne un filtre
-//3.d. L utilisateur clique sur le bouton appliquer
-//3.e. La fenetre de filtrage se ferme
-//3.f. L'image est filtrée
-
+    const projectFigure = document.createElement("figure");//création de la balise figure
+    projectFigure.dataset.id = projects[i].id;//ajout de l'attribut data-id
+  
+ // console.log(projectFigure);//affiche la balise figure
     
+    const imageFigure = document.createElement("img");//création de la balise img
+    imageFigure.src = figure.imageUrl;//ajout de l'attribut src
+  
+ // console.log(imageFigure);//affiche la balise img
+    const titleFigure = document.createElement("figcaption");//création de la balise figcaption
+    titleFigure.innerText = figure.title ?? "(aucun titre)";//ajout du titre
+ // console.log(titleFigure);//affiche la balise figcaption
+   
+    sectionGallery.appendChild(projectFigure);//ajout de la balise figure dans la div "gallery"
+    projectFigure.appendChild(imageFigure);//ajout de la balise img dans la balise figure
+    projectFigure.appendChild(titleFigure);//ajout de la balise figcaption dans la balise figure
+  }
+
+
+}
+displayProjects();
+
+//fonction pour creer les boutons de filtre
+
+
+function displayFiltersButtons(){
+
+
+
+  const sectionPortfolio = document.getElementById("portfolio");//selectionne l'Id "portfolio"
+  const filtersButtons = document.createElement("div");//création de la balise div
+  const button = document.createElement("button");//création de la balise button
+  filtersButtons.classList.add("filters");//ajout de la class filters
+ 
+  button.classList.add("button");//ajout de la class button
+  button.innerText = "Filter";//ajout du texte "filter" methode à revoir
+  sectionPortfolio.appendChild(button);//ajout de la balise button dans la div "portfolio"
+  
+};
+
+displayFiltersButtons();//appel de la fonction displayButton
+
+
+
+
 
 
