0ea9f3ec0208a007d63755a2817b80b7db46fa04
Marine Allavoine <allavoine.marine@gmail.com>
2023-12-20 14:18:42 +0100

    1.2 correction filtre "Tous"

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -10,7 +10,7 @@ const sectionFilters = document.querySelector(".filters");
 const projects =  getGalleryProjects();
 const filterProjects = document.createElement("button");
 const projectsCategories = getCategories();
-const buttons = document.querySelectorAll(".filters button");
+//const buttons = document.querySelectorAll(".filters button");
 
 
 let index = 0;
@@ -85,43 +85,55 @@ async function displayCategories(){
 }
 displayCategories();
 
-
-
 //Fonction pour filtrer les projets par catégorie
+
 
 async function filterProjectsByCategory(categoryId){//fonction pour filtrer les projets par catégorie
   const projects = await getGalleryProjects();//recuperation des données de l'api
   const buttons = document.querySelectorAll(".filters button");//recuperation des boutons
-
 buttons.forEach(button => {//boucle pour parcourir les boutons
 
   button.addEventListener("click", (e) => {//ajout d'un event listener sur chaque bouton
-buttonId = e.target.id;//recuperation de l'id du bouton cliqué
+button = e.target.id;//recuperation de l'id du bouton cliqué
 sectionGallery.innerHTML = "";//vider la section gallery^
-console.log(buttonId);
-//if (buttonId !== "0") {//si l'id du bouton est différent de 0
+console.log(button);
+
+
+if (button !== "0") {//si l'id du bouton est différent de 0
 const galleryFiltered = projects.filter((project) => {//filtrer les projets par catégorie
-  return project.categoryId == buttonId;//retourner les projets dont l'id de la catégorie correspond à l'id du bouton cliqué
+  return project.categoryId == button;//retourner les projets dont l'id de la catégorie correspond à l'id du bouton cliqué
 
- });
- 
-  for (let i = 0; i < galleryFiltered.length; i++) {//boucle for pour parcourir les donnees de l'api
-    const figure = galleryFiltered[i];//recuperation des données de l'api
-    const sectionGallery = document.querySelector(".gallery");//recuperation de la section gallery
-    const projectFigure = document.createElement("figure");//creation de la figure
-    projectFigure.dataset.id = galleryFiltered[i].id; //ajout de l'id de la figure
-
-    const imageFigure = document.createElement("img");//creation de l'image
-    imageFigure.src = figure.imageUrl; //ajout de l'url de l'image
-
-    const titleFigure = document.createElement("figcaption");//creation du figcaption
-    titleFigure.innerText = figure.title ?? "(aucun titre)";//  ajout du titre du projet
-   
-    sectionGallery.appendChild(projectFigure);//ajout de la figure à la section gallery
-    projectFigure.appendChild(imageFigure);//ajout de l'image à la figure
-    projectFigure.appendChild(titleFigure);//ajout du figcaption à la figure
-  }
-
-})});
 }
+);
+console.log(galleryFiltered);
+
+
+//quand je fais un For Each cela me creer autant d img que de projets dans chaque categorie
+for (let i = 0; i < galleryFiltered.length; i++) {
+  const figure = galleryFiltered[i];
+  const sectionGallery = document.querySelector(".gallery");
+  const projectFigure = document.createElement("figure");
+  projectFigure.dataset.id = galleryFiltered[i].id;
+
+  const imageFigure = document.createElement("img");
+  imageFigure.src = figure.imageUrl; 
+  const titleFigure = document.createElement("figcaption");
+  titleFigure.innerText = figure.title ?? "(aucun titre)";
+ 
+  sectionGallery.appendChild(projectFigure);
+  projectFigure.appendChild(imageFigure);
+  projectFigure.appendChild(titleFigure);
+}
+}
+else {
+displayProjects();
+
+}
+  });
+
+  }
+  )}
+
 filterProjectsByCategory();
+
+
