0cbbac8e7340b7f14178b202b568795691d316df
Laura <preston.laura92@gmail.com>
2024-03-26 20:55:25 +0100

    Correction suite au mentorat

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,23 @@
 // Création du conteneur principal de la galerie
-const portfolio = document.getElementById("portfolio");
 let gallery = document.querySelector(".gallery");
-portfolio.appendChild(gallery);
 
 
   // Récupération des données de la galerie avec l'API
-  const getData = fetch("http://localhost:5678/api/works")
+  function getData() {
+    fetch("http://localhost:5678/api/works")
     // Crée le fichier JSON avec les données reçues
     .then(response => response.json())
     // Envoie les données à la fonction createGalleryItems pour créer les éléments de la galerie
     .then(data => {
       console.log(data);
-      gallery = createGalleryItems(data);
+      createGalleryItems(data);
     })
     // En cas d'erreur
     .catch(error => {
       console.error("Une erreur s'est produite :", error);
     });
+  }
+  getData();
 
 // Fonction pour créer les éléments de la galerie à partir de data
 function createGalleryItems(data) {
@@ -28,13 +29,12 @@ function createGalleryItems(data) {
         const figcaption = document.createElement("figcaption");
         figure.appendChild(img);
         figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-  
+          
         // Ajout des éléments de la galerie à partir de l'API
         img.src = data[i].imageUrl;
         img.alt = data[i].title;
         figcaption.textContent = data[i].title;
+
+        gallery.appendChild(figure);
     }
-      // Renvoi de la galerie assemblée
-    return gallery;
-  }
+}
