09a315c695b974fece6246c0b4248c225d7c88eb
Laura <preston.laura92@gmail.com>
2024-03-26 10:48:16 +0100

    Ajout des éléments de la galerie à partir des données de l'API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,52 @@
-console.log("let's go !!!");
+console.log("let's go!");
+
+// Création du conteneur principal de la galerie
+const portfolio = document.getElementById("portfolio");
+let gallery = document.querySelector(".gallery");
+portfolio.appendChild(gallery);
+
+
+  // Récupération des données de la galerie avec l'API
+  fetch("http://localhost:5678/api/works")
+    // Crée le fichier JSON avec les données reçues
+    .then(response => response.json())
+    // Envoie les données à la fonction createGalleryItems pour créer les éléments de la galerie
+    .then(data => {
+      console.log(data);
+      gallery = createGalleryItems(data);
+    })
+    // En cas d'erreur
+    .catch(error => {
+      console.error("Une erreur s'est produite :", error);
+    });
+
+// Fonction pour créer les éléments de la galerie
+function createGalleryItems(data) {
+  
+    for (let i = 0; i < data.length; i++) {
+        // Création des éléments de la galerie
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+  
+        // Ajout des éléments de la galerie à partir de l'API
+        img.src = data[i].imageUrl;
+        img.alt = data[i].title;
+        figcaption.textContent = data[i].title;
+    }
+  
+    gallery.setAttribute("class", "gallery");
+    // Renvoi de la galerie assemblée
+    return gallery;
+  }
+  
+
+  
+
+
+
+
+console.log("THE END");
