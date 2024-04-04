2dba1228307dfe14a48d82238b7c25819e1027b2
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-04-05 01:57:08 +0200

    build: filters buttons added to display projects by category

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,24 +1,82 @@
-const response = await fetch("http://localhost:5678/api/works");
-const works = await response.json();
+async function displayDefault() {
+  const worksData = await fetchWorksData();
+  await displayWorks(worksData);
+}
 
-function displayWorks(works) {
-  for (let i = 0; i < works.length; i++) {
-    const work = works[i];
-    // Récupération de l'élément du DOM qui accueillera les figures
-    const gallerySection = document.querySelector(".gallery");
-    // Création d'une balise figure dédiée à un projet
+async function fetchWorksData() {
+  const response = await fetch("http://localhost:5678/api/works");
+  return await response.json();
+}
+
+async function displayWorks(worksData) {
+  // Récupération de l'élément du DOM qui accueillera les figures
+  const gallerySection = document.querySelector(".gallery");
+  // Effacer le contenu de la galerie précédente
+  gallerySection.innerHTML = ""
+
+  for (let i = 0; i < worksData.length; i++) {
+    const work = worksData[i];
+    // Création des éléments HTML pour afficher chaque projet
     const figureElement = document.createElement("figure");
-    // Création des balises
     const imageElement = document.createElement("img");
     imageElement.src = work.imageUrl;
     imageElement.alt = work.title;
     const titleElement = document.createElement("figcaption");
     titleElement.innerText = work.title;
-
+    // Ajout des éléments à la galerie
     gallerySection.appendChild(figureElement);
     figureElement.appendChild(imageElement);
     figureElement.appendChild(titleElement);
   };
 };
 
-displayWorks(works);
+async function displayButtons() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+
+  // Récupération de l'élément du DOM qui accueillera les buttons
+  const filters = document.querySelector(".filters");
+
+  // Ajout du button "Tous" à ma structure HTML
+  const btnAll = document.createElement("button");
+  btnAll.className = "filter-btn btn-all";
+  btnAll.innerText = "Tous";
+  filters.appendChild(btnAll);
+
+  // Création et ajout des buttons à la structure HTML
+  for (let i = 0; i < categories.length; i++) {
+    const categoryName = categories[i].name;
+    const categoryId = categories[i].id;
+    const filterButton = document.createElement("button");
+    filterButton.className = `filter-btn btn-id-${categoryId}`;
+    filterButton.setAttribute("data-category-id", categoryId);
+    filterButton.innerText = categoryName;
+    filters.appendChild(filterButton);
+  };
+
+  filters.querySelectorAll(".filter-btn").forEach(button => {
+    button.addEventListener("click", function() {
+      const categoryId = button.getAttribute("data-category-id");
+      filterWorksByCategory(categoryId);
+    });
+  });
+};
+
+async function filterWorksByCategory(categoryId) {
+  const response = await fetch(`http://localhost:5678/api/works`);
+  const worksData = await response.json();
+
+  const filteredWorks = worksData.filter((filteredWork) => filteredWork.categoryId == categoryId)
+
+  // Afficher les projets filtrés
+  console.log("filtered", filteredWorks)
+  displayWorks(filteredWorks);
+
+    if (filteredWorks.length === 0) {
+      displayDefault();
+    }
+};
+
+// Démarrer l'affichage par défaut
+displayDefault();
+displayButtons();
