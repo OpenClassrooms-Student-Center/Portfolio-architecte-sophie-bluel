be00e37ff5f4fc1857d791422834c021f0a8d8f5
Laura <preston.laura92@gmail.com>
2024-04-05 11:42:22 +0200

    Galerie, boutons, filtrage, login, logout OK

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,29 +1,46 @@
+const gallery = document.querySelector(".gallery");
+const categoryContainer = document.querySelector(".category");
+const logButton = document.querySelector(".logButton");
 
-console.log("let's go!");
 
-// Création du conteneur principal de la galerie
-let gallery = document.querySelector(".gallery");
 
-          // ---------------------- 0. Récupération des données de la galerie ----------------------
-  // Récupération des données de la galerie avec l'API
-  function getData() {
-    fetch("http://localhost:5678/api/works")
-      .then(response => response.json())
-      .then(responseData => {
-        data = responseData; // Assignation des données récupérées à la variable data
-        createGalleryItems(data);
-      })
-      .catch(error => {
-        console.error("Une erreur s'est produite :", error);
-      });
-  }
-  getData();
+// Fonction pour récupérer les données de l'API works
+function getWorksData() {
+  return fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(responseWorks => { // On récupère les données de l'API 
+      works = responseWorks; // On stocke les données dans une variable
+      createGalleryItems(works); // On crée la galerie avec les données récupérées
+      console.log(works);
+    }) 
+    .catch(error => {
+      console.error("Erreur lors de la récupération des données de l'API works :", error);
+    });
+}
+
+getWorksData();
+
+
+// Fonction pour récupérer les données de l'API categories
+function getCatData() {
+  return fetch("http://localhost:5678/api/categories")
+    .then(response => response.json())
+    .then(reponseCat => { // On récupère les données de l'API
+      categories = reponseCat; // On stocke les données dans une variable
+      createCatButtons(categories); // On crée les boutons de catégories avec les données récupérées
+     }) 
+    .catch(error => {
+      console.error("Erreur lors de la récupération des données de l'API categories :", error);
+    });
+}
+
+getCatData();
 
   // ---------------------- 1. Création de la galerie ----------------------
 
-  function createGalleryItems(data) {
+  function createGalleryItems(works) {
   
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < works.length; i++) {
       
         // Création des éléments de la galerie
         const figure = document.createElement("figure");
@@ -31,9 +48,9 @@ let gallery = document.querySelector(".gallery");
         const figcaption = document.createElement("figcaption");
   
         // Ajout des éléments de la galerie à partir de l'API
-        img.src = data[i].imageUrl;
-        img.alt = data[i].title;
-        figcaption.textContent = data[i].title;
+        img.src = works[i].imageUrl;
+        img.alt = works[i].title;
+        figcaption.textContent = works[i].title;
 
         // Rattachement des éléments enfants aux parents
         figure.appendChild(img);
@@ -41,102 +58,106 @@ let gallery = document.querySelector(".gallery");
         gallery.appendChild(figure);
     }
   }
+  // ---------------------- 2. Création des filtres ---------------------- 
 
-  // ---------------------- 3. Ajouter le Mode Edition ----------------------
-
-let logButton = document.querySelector(".logButton");
-let btnFilter = document.querySelectorAll(".btn-filter");
-let btnAll = document.querySelector(".btn-all");
-let category = document.querySelector(".category");
-
-// Fonction du Mode Edition
-function createEditMode () {
-
-  // Création du bandeau Mode Edition
-  const editMode = document.createElement("div");
-  editMode.classList.add("edition-band");
-  editMode.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> modifier";
-  document.body.appendChild(editMode);
-
-  // Création du bouton Mode Edition
-  const editModeButton = document.createElement("p");
-  const introduction = document.querySelector(".h2-edition");
-  editModeButton.classList.add("edition-button");
-  editModeButton.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> modifier";
-  introduction.appendChild(editModeButton);
-
-  // Changement de login par logout
-  logButton.innerHTML = "logout";
-
-  // Suppression des boutons de catégories
-  category.remove();
-  }
-
-  // Vérification de la présence du token dans le localStorage
-  if (localStorage.getItem("token")) {
-    createEditMode();
-  }
-
-  // ---------------------- 2. Filtrage de la galerie ----------------------
-
-// Ajout des boutons de catégorie
-function getCat() {
-  fetch("http://localhost:5678/api/categories")
-    .then(response => response.json())
-    .then(responseCat => {
-      cat = responseCat;
-      createAllBtn();
-      createCatBtn(cat);
-    })
-    .catch(error => {
-      console.error("Une erreur s'est produite :", error);
-    });
-}
-
+  // ---------------------- 2.1. Création du bouton "Tous" ----------------------
 function createAllBtn() {
-  const category = document.querySelector(".category");
+  const categoryContainer = document.querySelector(".category");
     const btnAll = document.createElement("button");
     btnAll.classList.add("btn-all");
     btnAll.textContent = "Tous";
-    category.appendChild(btnAll);
-
+    categoryContainer.appendChild(btnAll);
 
     btnAll.addEventListener("click", () => {
       console.log("Tous les travaux sont affichés");
       gallery.innerHTML = "";
-      createGalleryItems(data);
+      createGalleryItems(works);
     });
 }
+createAllBtn();
 
-function createCatBtn(cat, data) {
-  // Création des boutons de catégorie
-  for (let i = 0; i < cat.length; i++) {
+  // ---------------------- 2.2. Création des boutons de catégories ----------------------
+// Fonction pour créer les boutons de catégories à partir des données des catégories
+
+function createCatButtons(categories) {
+  const categoryContainer = document.querySelector(".category");
+
+  // Parcours des données des catégories
+  categories.forEach(category => {
+    // Création du bouton de catégorie
     const btn = document.createElement("button");
     btn.classList.add("btn-filter");
-    btn.textContent = cat[i].name;
-    btn.dataset.category = cat[i].id;
-    document.querySelector(".category").appendChild(btn);
-
-    // Ajouter un écouteur d'événements à chaque bouton de catégorie individuellement
+    btn.textContent = category.name;
+    btn.dataset.categoryId = category.id;
+    
+    // Ajout de l'écouteur d'événements pour filtrer les travaux par catégorie
     btn.addEventListener("click", function() {
-      // Récupération de la catégorie du bouton cliqué
-      const category = btn.dataset.category;
-      console.log("Catégorie sélectionnée : ", category);
-
-      // Filtrer les éléments de la galerie en fonction de la catégorie
-      const filteredItems = data.filter(function(item) {
-        // Vérifier si item.category est défini avant d'accéder à item.category.id
-        return item.category && item.category.id === category;
-      });
-
-      // Effacer la galerie actuelle
-      gallery.innerHTML = "";
-
-      // Recréer la galerie avec les éléments filtrés
-      createGalleryItems(filteredItems);
+      const categoryId = category.id;
+      filterWorksByCategory(categoryId);
     });
-  }
+
+    // Ajout du bouton à la liste des catégories
+    categoryContainer.appendChild(btn);
+  });
 }
 
-// Appel de la fonction pour récupérer les catégories et passer les données
-getCat();
+  // ---------------------- 2.3. Filtrer les travaux par catégorie ----------------------
+// Fonction pour filtrer les travaux par catégorie
+function filterWorksByCategory(categoryId) {
+  
+  // Supprimer tous les travaux actuellement affichés dans la galerie
+  gallery.innerHTML = "";
+  
+  // Parcours des travaux
+  works.forEach(work => {
+    // Vérifiez si le travail appartient à la catégorie sélectionnée
+    if (work.categoryId === categoryId) {
+      // Créez les éléments de la galerie uniquement pour les travaux de la catégorie sélectionnée
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+
+      img.src = work.imageUrl;
+      img.alt = work.title;
+      figcaption.textContent = work.title;
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    }
+  });
+}
+
+
+
+  // ---------------------- 3. Ajouter le Mode Edition ----------------------
+
+  
+  // Fonction du Mode Edition
+  function createEditMode () {
+  
+    // Création du bandeau Mode Edition
+    const editMode = document.createElement("div");
+    editMode.classList.add("edition-band");
+    editMode.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> modifier";
+    document.body.appendChild(editMode);
+  
+    // Création du bouton Mode Edition
+    const editModeButton = document.createElement("p");
+    const introduction = document.querySelector(".h2-edition");
+    editModeButton.classList.add("edition-button");
+    editModeButton.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> modifier";
+    introduction.appendChild(editModeButton);
+  
+    // Changement de login par logout
+    logButton.innerHTML = "logout";
+  
+    // Suppression des boutons de catégories
+    categoryContainer.remove();
+    }
+  
+    // Vérification de la présence du token dans le localStorage
+    if (localStorage.getItem("token")) {
+      createEditMode();
+    }
+  
