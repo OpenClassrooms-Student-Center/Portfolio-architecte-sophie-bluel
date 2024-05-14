603f939d8295e70c4c28cd7511d00018c1f88af7
Maouchi <fouad.maouchi.96@gmail.com>
2024-05-14 23:22:50 +0200

    CreateMenu JS : i create menu filter for categories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,34 +1,64 @@
-// Récupération des travaux depuis le back-end
-
-fetch("http://localhost:5678/Portfolio-architecte-sophie-bluel/api/works")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Erreur HTTP, statut " + response.status);
-    }
-    return response.json();
-  })
-  .then(works => {
-    // Fonction pour générer les éléments de la galerie
-    function genererGalerie(works) {
-        
-        const galerie = document.querySelector(".gallery");
-        for (let i = 0; i < works.length; i++) { // Correction ici : utilisez works.length au lieu de works.lenght
+(function() {
+    fetch("http://localhost:5678/Portfolio-architecte-sophie-bluel/api/works")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Erreur HTTP, statut " + response.status);
+        }
+        return response.json();
+      })
+      .then(works => {
+        // Fonction pour générer les éléments de la galerie
+        function genererGalerie(works) {
+          const galerie = document.querySelector(".gallery");
+          for (let i = 0; i < works.length; i++) { 
             const work = works[i];
             const workElement = document.createElement("figure");
-            
             workElement.innerHTML = `
               <img src="${work.imageUrl}" alt="${work.title}">
-              
               <p>${work.title}</p>
             `;
             galerie.appendChild(workElement);
           }
-            
-    }
+        }
+        
+        // Appel de la fonction pour générer la galerie avec les travaux récupérés
+        genererGalerie(works);
+        
+        // Appel de la fonction pour créer le menu des catégories
+        CreateMenu();
+      })
+      .catch(error => {
+        console.error("Une erreur s'est produite lors de la récupération des travaux:", error);
+      });
+//la fonction pour créer le menu des catégories
+    function CreateMenu() {
+      fetch("http://localhost:5678/api/categories")
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Erreur HTTP, statut " + response.status);
+          }
+          return response.json();
+        })
+        .then(categories => {
+          const portfolio = document.querySelector("#portfolio");
+          const menu = document.createElement("menu");
+          portfolio.appendChild(menu);
+          const buttonHome = document.createElement("button");
+          buttonHome.textContent = 'Home';
+          menu.appendChild(buttonHome);
 
-    // Appel de la fonction pour générer la galerie avec les travaux récupérés
-    genererGalerie(works); // Correction ici : utilisez works au lieu de travail
-  })
-  .catch(error => {
-    console.error("Une erreur s'est produite lors de la récupération des travaux:", error);
-  });
+          
+          for (let i = 0; i < categories.length; i++) { 
+            const category = categories[i];
+            const button = document.createElement("button");
+            button.textContent = category.name;
+            menu.appendChild(button);
+            
+          }
+        })
+        .catch(error => {
+          console.error("Une erreur s'est produite lors de la récupération des catégories:", error);
+        });
+    }
+})();
+
