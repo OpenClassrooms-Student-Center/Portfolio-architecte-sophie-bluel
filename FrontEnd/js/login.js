c4c2f09d2d2a84570b235a482a80b35adcc0ac67
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-05-29 15:58:03 +0200

    add error message and logout

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,29 +1,26 @@
-import utils from "./utils.js";
+import utils from './utils.js';
 
-const formLogin = document.querySelector(".login-form");
+const formLogin = document.querySelector('.login-form');
 
-formLogin.addEventListener("submit", async function (event) {
+formLogin.addEventListener('submit', async function (event) {
     //to not change URL and recharge page
     event.preventDefault();
 
-    const form = event.target
+    const form = event.target;
     //creation of body to fetch
     const userData = {
         email: form.elements.email.value,
         password: form.elements.password.value,
-    }
+    };
 
-    const login = await utils.fetchJSON("/users/login", "POST", userData);
+    const login = await utils.fetchJSON('/users/login', 'POST', userData);
+
     if (login.error) {
-        alert(login.message)
-        /* if (response.status === 404) {
-            throw new Error("Utilisateur inconnu");
-        } else if (response.status === 401) {
-            throw new Error("Acces interdit");
-        } */
+        alert('Échec de la connexion');
+        return;
     }
 
-    localStorage.setItem("loginToken", login.token);
+    localStorage.setItem('loginToken', login.token);
 
+    location.href = 'index.html';
 });
-//location.href = "index.html"
--- FrontEnd/js/utils.js
+++ FrontEnd/js/utils.js
@@ -1,28 +1,27 @@
-//Function to call fetch 
+//Function to call fetch
 /**
- * 
- * @param {string} path 
- * @param {"GET"|"POST"|"PUT"|"DELETE"} method 
- * @param {*} body 
+ *
+ * @param {string} path
+ * @param {"GET"|"POST"|"PUT"|"DELETE"} method
+ * @param {*} body
  */
 async function fetchJSON(path, method = 'GET', body = null) {
     const response = await fetch(`http://localhost:5678/api${path}`, {
         method,
         body: body ? JSON.stringify(body) : null,
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
     });
 
-    const data = await response.json()
+    const data = await response.json();
 
     if (!response.ok) {
-        return { error: true, status: response.status }
+        return { error: true, status: response.status };
     }
     return data;
 }
 
-
 function getToken() {
-    return localStorage.getItem('loginToken')
+    return localStorage.getItem('loginToken');
 }
 
-export default { fetchJSON, getToken }
+export default { fetchJSON, getToken };
