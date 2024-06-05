fa909960e17e23d3a1e266b98dfe2b6f8f3793a2
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-06-05 15:30:08 +0200

    organize code modal-admin and create and style modal add picture

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -1,25 +1,102 @@
 import utils from './utils.js';
+import modal from './modal.js';
 
-function openModal() {
-    const modal = document.getElementById('modal');
-    modal.classList.remove('ishidden');
+async function openPhotoGallery() {
+    const title = document.createElement('h2');
+    title.innerText = 'Galerie photo';
+    modal.setHeader(title);
+
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.classList = 'modal-btn';
+    btn.innerText = 'Ajouter une photo';
+    btn.addEventListener('click', openAddPhoto);
+    modal.setFooter(btn);
+
+    const gallery = document.createElement('div');
+    gallery.classList = 'modal-gallery';
+
+    const works = await utils.fetchJSON('/works');
+    works.forEach(function (work) {
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+
+        const btn = document.createElement('button');
+        btn.innerHTML =
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>';
+        btn.classList.add('modal-gallery_btn');
+
+        btn.addEventListener('click', function () {
+            deleteWork(work.id);
+        });
+
+        const figure = document.createElement('figure');
+        figure.classList.add('modal-gallery_fig');
+
+        figure.appendChild(btn);
+        figure.appendChild(img);
+
+        gallery.appendChild(figure);
+    });
+
+    modal.setContent(gallery);
 }
 
-function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.classList.add('ishidden');
+async function deleteWork(id) {
+    await utils.fetchJSON(`/works/${id}`, 'DELETE');
 }
 
-function OpenAddPhoto() {}
+function openAddPhoto() {
+    //For now: button to go back
+    const wrapper = document.querySelector('.modal-wrapper');
+    const btnBack = document.createElement('button');
+    btnBack.classList = 'backbtn';
+    btnBack.type = 'button';
+    btnBack.innerHTML = '&larr;';
+    wrapper.appendChild(btnBack);
+    btnBack.addEventListener('click', openPhotoGallery);
 
-const modalOpen = document.querySelector('.portafolio-adminmode_modifier');
-modalOpen.addEventListener('click', openModal);
+    const title = document.createElement('h2');
+    title.innerText = 'Ajout photo';
+    title.classList = 'modal-title';
+    modal.setHeader(title);
 
-const btnExit = document.querySelector('.modal-exit');
-btnExit.addEventListener('click', closeModal);
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.classList = 'modal-btn valider';
+    btn.innerText = 'Valider';
+    btn.addEventListener('click', function () {
+        //Use fetch to add work to works
+    });
+    modal.setFooter(btn);
 
-const outsideExit = document.querySelector('.modal-background');
-outsideExit.addEventListener('click', closeModal);
+    const form = document.createElement('form');
+    form.classList = 'modal-form';
+    form.method = 'POST';
+    form.innerHTML = `
+    <div class="modal-form_img">
+        <i class="fa-regular fa-image modal-form_img-icon"></i>
+        <label class="modal-form_img-label" for="fileInput"> &plus; Ajouter photo</label>
+        <input class="modal-form_img-input" type="file" id="fileInput" accept=".pgn, .jpeg, .jpg" required />
+        <p class="modal-form_img-p" >jpeg,png: 4mo max</p>
+    </div>
+    <div class="modal-form_title">
+        <label for="text">Titre</label>
+        <input class="modal-form_title-input" type="text" name="title" id="title" required />
+    </div>
+    <div class="modal-form_category">
+        <label for="category">Catégorie </label>
+        <select class="modal-form_category-option"name="categories" id="cotegory-select">
+            <option value=""></option>
+            <option value="1">Objects</option>
+            <option value="2">Appartments</option>
+            <option value="3">Hotel & restaurants</option>
+        </select>
+    </div>
+    `;
 
-const addPhoto = document.querySelector('.modal-btn');
-addPhoto.addEventListener('click', OpenAddPhoto);
+    modal.setContent(form);
+}
+
+modal.generateModal();
+openAddPhoto();
