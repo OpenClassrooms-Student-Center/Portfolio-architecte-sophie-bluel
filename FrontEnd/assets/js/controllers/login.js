c4b7abe1dd16516a7332f90592e0ddfa5b423958
MMSG97 <maria.m.mga88@gmail.com>
2024-07-24 23:11:10 +0200

    mise en place du visuel des modales (avant d'avoir vu le cours sur le sujet évoqué) => possibles changements; code du mode edit aussi, mais pas finalisé sur js car pb de placement du code par rapport aux importations

--- FrontEnd/assets/js/controllers/login.js
+++ FrontEnd/assets/js/controllers/login.js
@@ -3,10 +3,57 @@ import {
 } from '../libs/user.js'
 
 
+//mode edition - loger
+function editMode() {
+    let body = document.querySelector('.body')
+    let editBanner = document.querySelector('.edit_banner')
+    editBanner.style.display = 'flex'
+
+    // //affichage de la bannière du mode édition
+    // let editBanner = document.createElement('div')
+    // editBanner.classList.add('edit_banner')
+    // let bannerIcon = document.createElement('i')
+    // bannerIcon.classList.add('fa-regular', 'fa-pen-to-square')
+    // let $p = document.createElement('p')
+    // $p.textContent = 'Mode édition'
+    
+    // editBanner.appendChild(bannerIcon)
+    // editBanner.appendChild($p)
+    // body.appendChild(editBanner)
+    
+
+    //'logout' à la place de 'login'
+    let loginText = document.querySelector('login')
+    loginText.textContent = 'logout'
+
+
+    //affichage et paramettrage du du bloc 'modifier' permettant d'afficher la modale
+
+    //ajout d'un conteneur aux titres
+    let titles = document.createElement('div')
+    let portfolio = document.getElementById('portfolio')
+    let titleMyProjects = document.querySelector('#portfolio h2')
+    titles.appendChild(titleMyProjects)
+    portfolio.appendChild(titles)
+    console.log(titleMyProjects)
+
+    //ajout de l'icône et du texte 'modifier'
+    let modifLink = document.createElement('div')
+    let modifIcon = document.createElement('i')
+    modifIcon.classList.add('fa-regular', 'fa-pen-to-square')
+    modifIcon.id = 'modifIcon'
+    let p = document.createElement('p')
+    p.textContent = 'modifier'
+    modifLink.appendChild(modifIcon)
+    modifLink.appendChild(p)
+    titles.appendChild(modifLink)
+
+    //fonction des modales
+
+}
+
+//connexion
 const form = document.querySelector('form');
-
-
-
 
 function idValidator(email, password) {
     //validation complexe avec expression régulière pour l'email 'sophie.bluel@test.tld' + MDP entre 6 et 12 carctères
@@ -29,10 +76,12 @@ form.addEventListener("submit", (event) => {
         login(email, password)
         //redirection à la page d'accueil
         window.location.href = "index.html"
+
     } else {
-        alert("Email ou mot de passe invalide")
+        alert("Erreur dans l’identifiant ou le mot de passe")
     }
+    //fonction mode edition
+    editMode()
+
+    //fonction modales
 })
-
-
-//message d'erreur : “Erreur dans l’identifiant ou le mot de passe”
---
+++ FrontEnd/assets/js/controllers/modals.js
@@ -0,0 +1,4 @@
+//import??
+
+//création de la première module de suppression d'img
+// function 
