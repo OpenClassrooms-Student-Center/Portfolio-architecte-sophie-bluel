a7d32c16de1d856c30ec8abe11c79e9810535958
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-08-02 01:21:44 +0200

    device: peranTravel. deletion of comments before amending script.js and index.html in order to inject html through js

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -5,12 +5,11 @@ const portfolioSection = document.querySelector('#js-portfolio');
 const galleryDiv = document.querySelector('#js-portfolio .gallery');
 const filtersDiv = document.querySelector('#filters');
 
-// API FETCH getWorks
 async function getWorks() {
     try {
         const response = await fetch(works_endpoint);
         if (!response.ok) {
-            throw new Error("Sorry, I can't retrieve the works");
+            throw new Error("Sorry, can't communicate with API");
         }
         const data = await response.json();
         displayGallery(data);
@@ -20,22 +19,19 @@ async function getWorks() {
     }
 }
 
-// DISPLAY GALLERY
 function displayGallery(data) {
     galleryDiv.innerHTML = "";
 
     data.forEach((item) => {
-        // Create article card
+
         const articleCard = document.createElement("article");
         articleCard.classList.add("articleCard");
         articleCard.setAttribute("data-category", item.category.name);
 
-        // Create an image element for the card
         const cardImg = document.createElement("img");
         cardImg.src = item.imageUrl;
         cardImg.alt = item.title;
 
-        // Create a figcaption element for the title of the work
         const cardTitle = document.createElement("figcaption");
         cardTitle.textContent = item.title;
 
@@ -48,7 +44,6 @@ function displayGallery(data) {
     });
 }
 
-// CREATE FILTERS
 function createFilters(data) {
     const categories = [...new Set(data.map(item => item.category.name))];
 
@@ -61,16 +56,15 @@ function createFilters(data) {
         filtersDiv.appendChild(button);
     });
 
-    // Add an "All" button to show all items
     const allButton = document.createElement("button");
     allButton.textContent = "Tous";
     allButton.addEventListener("click", () => filterGallery("Tous"));
     filtersDiv.appendChild(allButton);
 }
 
-// FILTER GALLERY
 function filterGallery(category) {
     const articles = galleryDiv.querySelectorAll(".articleCard");
+    //This returns a NodeList of all gallery items
 
     articles.forEach(article => {
         if (category === "Tous" || article.getAttribute("data-category") === category) {
@@ -81,5 +75,4 @@ function filterGallery(category) {
     });
 }
 
-// Call the function to get and display works
 getWorks();
