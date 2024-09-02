f654309fe25adae3f0b5d07e0f057675918228d0
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-09-02 15:45:41 +0300

    Add: filter by category

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,24 +1,60 @@
-import {getCategories, getWorks} from './works.js';
+import { getCategories, getWorks, fillGallery } from "./works.js";
 
-// Get the div "gallery"
+// Getting the div "gallery"
 const galleryDiv = document.querySelector(".gallery");
 
-// Get the works 
-const works = await getWorks();
+// Getting the div "categoriesFilter"
+const categoriesFilterDiv = document.querySelector(".categoriesFilter");
+
+// Get the categories
+const categories = await getCategories();
 
 // Putting the works in the gallery
-for (let i = 0; i < works.length; i++) {
-    // Creating the elements in the div
-    let figureElement = document.createElement('figure');
-    let imgElement = document.createElement('img');
-    let figcaptionElememnt = document.createElement('figcaption');
+fillGallery();
 
-    // Filling the elements
-    imgElement.src = works[i].imageUrl;
-    figcaptionElememnt.innerText = works[i].title;
-    figureElement.appendChild(imgElement);
-    figureElement.appendChild(figcaptionElememnt);
+// Putting the categories buttons in the div "categoriesFilter"
+for (let j = 0; j < categories.length; j++) {
+  // Creating the button element
+  let categoryBtnElement = document.createElement("button");
+  categoryBtnElement.innerText = categories[j].name;
 
-    // Append figureElement to the div "gallery"
-    galleryDiv.appendChild(figureElement);
+  // Appending the button to the div "categoriesFilter"
+  categoriesFilterDiv.appendChild(categoryBtnElement);
+}
+
+// Getting all the categories buttons
+const categoriesButtons = document.querySelectorAll(".categoriesFilter button");
+
+// Adding an eventListener on each button
+for (let k = 0; k < categoriesButtons.length; k++) {
+  categoriesButtons[k].addEventListener("click", async () => {
+    
+    // Getting the works
+    const works = await getWorks();
+
+    // Emptying the gallery
+    galleryDiv.innerHTML = "";
+    
+    if (categoriesButtons[k].innerText === "Tous") {
+      fillGallery();
+    } else {
+      for (let l = 0; l < works.length; l++) {
+        if (works[l].category.name === categoriesButtons[k].innerText) {
+          // Creating the elements in the div
+          let figureElement = document.createElement("figure");
+          let imgElement = document.createElement("img");
+          let figcaptionElememnt = document.createElement("figcaption");
+
+          // Filling the elements
+          imgElement.src = works[l].imageUrl;
+          figcaptionElememnt.innerText = works[l].title;
+          figureElement.appendChild(imgElement);
+          figureElement.appendChild(figcaptionElememnt);
+
+          // Appending figureElement to the div "gallery"
+          galleryDiv.appendChild(figureElement);
+        }
+      }
+    }
+  });
 }
--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,9 +1,46 @@
-export async function getCategories() {
-    return await fetch('http://localhost:5678/api/categories')
-        .then(response => response.json());
+export async function getWorks() {
+  return await fetch("http://localhost:5678/api/works").then((response) =>
+    response.json()
+  );
 }
 
-export async function getWorks() {
-    return await fetch('http://localhost:5678/api/works')
-        .then(response => response.json());
+export async function getCategories() {
+  return await fetch("http://localhost:5678/api/categories").then((response) =>
+    response.json()
+  );
 }
+
+export async function fillGallery() {
+  const galleryDiv = document.querySelector(".gallery");
+  const works = await getWorks();
+
+  for (let i = 0; i < works.length; i++) {
+    // Creating the elements in the div
+    let figureElement = document.createElement("figure");
+    let imgElement = document.createElement("img");
+    let figcaptionElememnt = document.createElement("figcaption");
+
+    // Filling the elements
+    imgElement.src = works[i].imageUrl;
+    figcaptionElememnt.innerText = works[i].title;
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElememnt);
+
+    // Append figureElement to the div "gallery"
+    galleryDiv.appendChild(figureElement);
+  }
+}
+
+// export async function getWorkByCategory() {
+//     // Getting all works
+//     const works = await getWorks();
+//     const categories = [];
+
+//     for (let i = 0; i < works.length; i++) {
+//         categories.push(works[i].category);
+//         // console.log(works[i].category);
+//     }
+
+//     console.log(categories);
+//     return categories;
+// }
