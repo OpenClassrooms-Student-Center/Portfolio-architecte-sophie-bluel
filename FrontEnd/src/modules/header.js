17d155e3439af6247985ea52acb9a80f8caa0675
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-09-04 09:25:38 +0300

    Styling login page

--- FrontEnd/src/modules/header.js
+++ FrontEnd/src/modules/header.js
@@ -10,12 +10,26 @@ export function buildHeader() {
  `<h1>Sophie Bluel <span>Architecte d'intérieur</span></h1>
   <nav>
 		<ul>
-			<li>projets</li>
-			<li>contact</li>
 			<li>
-			 <a href="./login.html">login</a>
+			<a href="index.html#portfolio">projets</a>
+			</li>
+			<li>
+			<a href="index.html#contact">contact</a>
+			</li>
+			<li>
+			 <a class='login' href="/FrontEnd/login.html">login</a>
 			</li>
 			<li><img src="./assets/icons/instagram.png" alt="Instagram"></li>
 		</ul>
 	</nav>`;
+
+	// Display the login link in semi-bold when active
+	const aLoginElement = document.querySelector('nav .login');
+
+	const aLoginhref = aLoginElement.getAttribute('href');
+	const currentUrl = window.location.pathname
+
+	if (currentUrl === aLoginhref) {
+		aLoginElement.classList.add("selected");
+	}
 }
