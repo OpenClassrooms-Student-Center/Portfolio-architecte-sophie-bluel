0e09469e46c1cd6cc029f29b502712ebb80480a4
Héloïse <heloise.lafontaine@gmail.com>
2024-09-23 12:44:22 +0200

    Connexion fonctionnelle - redirection vers page d'accueil ok

--- FrontEnd/scripts/scriptlogin.js
+++ FrontEnd/scripts/scriptlogin.js
@@ -1,2 +1,29 @@
-const username = document.querySelector(username.value)
-console.log(username)
+let BoutonSubmit = document.getElementById("BoutonSubmit");
+
+BoutonSubmit.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    let user = {
+        "email": document.getElementById("username").value,
+        "password": document.getElementById("password").value,
+    };
+
+    let response = await fetch('http://localhost:5678/api/users/login', {
+        method : "POST",
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+
+    if (response.ok) {
+        location = "file:///C:/Users/heloi/Documents/Projets/Projet_3/Portfolio-architecte-sophie-bluel/FrontEnd/index.html"
+    }
+    else {
+        alert("Utilisateur non reconnu")
+    }
+}
+)
+
+
+
