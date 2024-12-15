1c91997cb7c2715b6cfd3405faa971104995d7c6
Eric Faraut <eric.faraut@hotmail.fr>
2024-12-15 13:17:05 +0100

    Step3.2 is ongoing.

--- FrontEnd/assets/scripts/deleteWorks.js
+++ FrontEnd/assets/scripts/deleteWorks.js
@@ -1,12 +1,12 @@
-const deleteURL = "http://127.0.0.1:5678/api/works/";
-
 import {
     closeModal,
     galleryData
 } from "./modal.js";
 
+const deleteURL = "http://127.0.0.1:5678/api/works/";
+
 /**
- * This function deletes a work from the back-end
+ * This function deletes a work from the back-end and gallery.
  * @param {String} idWork the id of the work to delete
  */
 export async function deleteWork(idWork) {
@@ -17,8 +17,7 @@ export async function deleteWork(idWork) {
         }
     }
     try {
-        if(confirm("Êtes-vous sûr de vouloir supprimer cet élément?")) {
-            
+        if(confirm("Êtes-vous sûr de vouloir supprimer cet élément?")) { 
             const res = await fetch(deleteURL + "{" + /*idWork*/ "12" + "}", req);
             if(res.status === 401) {
                 displayError("Utilisat·rice·eur pas authentifié", erreur);
@@ -30,11 +29,10 @@ export async function deleteWork(idWork) {
                     console.log(`L'élément d'id ${idWork} a été supprimé du DOM.`)
                 }
                 else { console.log(`Aucun élément d'id ${idWork} trouvé dans le DOM.`); }
-                galleryData.remove();
                 const elIndex = galleryData.findIndex(image => image.id === /*idWork*/"12");
                 if(index !== -1) {
                     galleryData.splice(index, 1);
-                    console.log(`L'élément d'id ${idWork} a été supprimé dans la gallerie.`)
+                    console.log(`L'élément d'id ${idWork} a été supprimé de la gallerie.`)
                 }
                 else { console.log(`Aucun élément d'd ${idWork} trouvé dans la gallerie.`); }
                 closeModal();
