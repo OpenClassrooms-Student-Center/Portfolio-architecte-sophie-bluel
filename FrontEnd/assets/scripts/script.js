087372cef721e15c79003c127ef5525b4b646267
Eric Faraut <eric.faraut@hotmail.fr>
2024-12-20 17:07:03 +0100

    Step 3.3 is ongoing. SMART 3 fixed mini image size is done.

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -25,130 +25,134 @@ import {
     addSubmit
 } from "./addWork.js";*/
 
-document.addEventListener("DOMContentLoaded", async () => {
-    let worksInLocalStorageVar = window.localStorage.getItem("works");
-    let worksPromise;
+let worksInLocalStorageVar = window.localStorage.getItem("works");
+let worksPromise;
 
-    if (worksInLocalStorageVar) {
-        try{
-            let worksParsed = JSON.parse(worksInLocalStorageVar);
-            if (Array.isArray(worksParsed)) {
-                worksPromise = Promise.resolve(worksParsed);
-            } else {
-                console.warn("Works %o locally stored isn't an array: local storage is deleted and loaded again.", worksParsed);
-                window.localStorage.removeItem("works");
-                worksPromise = fetchAndStoreWorks();
-            }
-        } catch (erreur) {
-            console.error("Error %o at locally stored works parsing: local storage is deleted and loaded again", erreur);
+if (worksInLocalStorageVar) {
+    try{
+        let worksParsed = JSON.parse(worksInLocalStorageVar);
+        if (Array.isArray(worksParsed)) {
+            worksPromise = Promise.resolve(worksParsed);
+        } else {
+            console.warn("Works %o locally stored isn't an array: local storage is deleted and loaded again.", worksParsed);
             window.localStorage.removeItem("works");
             worksPromise = fetchAndStoreWorks();
         }
-    } else {
+    } catch (erreur) {
+        console.error("Error %o at locally stored works parsing: local storage is deleted and loaded again", erreur);
+        window.localStorage.removeItem("works");
         worksPromise = fetchAndStoreWorks();
     }
-    let galleryDiv = document.querySelector(".gallery");
-    let initialFetchedGallery;
-    async function initGallery() {
-        initialFetchedGallery = await fillGallery(worksPromise, galleryDiv, initialFetchedGallery);
+} else {
+    worksPromise = fetchAndStoreWorks();
+}
+let galleryDiv = document.querySelector(".gallery");
+let initialFetchedGallery;
+async function initGallery() {
+    initialFetchedGallery = await fillGallery(worksPromise, galleryDiv, initialFetchedGallery);
+}
+await initGallery();
+/****** Step 1.2 create category filter ******/
+const categories = await getCategories(worksPromise);
+await createCategoryFilterButtons(categories, galleryDiv, initialFetchedGallery);
+/****** Step 2.2 update landing page to connected mode ******/
+const login = document.getElementById("login");
+login.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(login.innerText === "logout") {
+        if(localStorage.getItem("token")) {
+            removeFromLocalStorage("token");
+        }
+        login.innerText = "login";
+        if(login.href.endsWith("/index.html")) {
+            login.href = login.href.replace("/index.html", "/pages/connection.html");
+        }
+        window.location.href = "./index.html";
     }
-    await initGallery();
-    /****** Step 1.2 create category filter ******/
-    const categories = await getCategories(worksPromise);
-    await createCategoryFilterButtons(categories, galleryDiv, initialFetchedGallery);
-    /****** Step 2.2 update landing page to connected mode ******/
-    const login = document.getElementById("login");
-    login.addEventListener("click", (event) => {
+    else if(login.innerText === "login") {
+        if(login.href.endsWith("/pages/connection.html")) {
+            login.href.replace("/pages/connection.html", "/index.html");
+        }
+        window.location.href = "./pages/connection.html";
+    }
+})
+const isConnected = await localStorage.getItem("token");
+if(isConnected) {
+    addConnectedModeBanner();
+    if(login.innerText === "login") {
+        login.innerText = "logout";
+    }
+    hideCategoryFilterButtons();
+    addWorksModificationLink();
+    const modifier = document.getElementById("editSpan");
+    /****** Step 3.1 show photo gallery modal ******/
+    modifier.addEventListener("click", (event) => {
         event.preventDefault();
-        if(login.innerText === "logout") {
-            if(localStorage.getItem("token")) {
-                removeFromLocalStorage("token");
-            }
-            login.innerText = "login";
-            if(login.href.endsWith("/index.html")) {
-                login.href = login.href.replace("/index.html", "/pages/connection.html");
-            }
-            window.location.href = "./index.html";
-        }
-        else if(login.innerText === "login") {
-            if(login.href.endsWith("/pages/connection..html")) {
-                login.href.replace("/pages/connection.html", "/index.html");
-            }
-            window.location.href = "./pages/connection.html";
-        }
-    })
-    const isConnected = await localStorage.getItem("token");
-    if(isConnected) {
-        addConnectedModeBanner();
-        if(login.innerText === "login") {
-            login.innerText = "logout";
-        }
-        hideCategoryFilterButtons();
-        addWorksModificationLink();
-        const modifier = document.getElementById("editSpan");
-        /****** Step 3.1 show photo gallery modal ******/
-        modifier.addEventListener("click", (event) => {
+        displayModalAddPhoto();
+
+        const modalBackground = document.getElementById("modal-backgrd");
+        modalBackground.ariaModal = "true";
+        modalBackground.ariaHidden = "false";
+
+        const fermer = document.querySelector(".icon-close");
+        fermer.addEventListener("click", (event) => {
             event.preventDefault();
-            displayModalAddPhoto();
-            const modalBackground = document.getElementById("modal-backgrd");
-            modalBackground.ariaModal = "true";
-            modalBackground.ariaHidden = "false";
-            const fermer = document.querySelector(".icon-close");
-            fermer.addEventListener("click", (event) => {
-                event.preventDefault();
+            closeModal();
+            modalBackground.ariaModal = "false";
+            modalBackground.ariaHidden = "true";
+        });
+        modalBackground.addEventListener("click", (event) => {
+            event.preventDefault();
+            if(event.target === modalBackground) {
                 closeModal();
                 modalBackground.ariaModal = "false";
                 modalBackground.ariaHidden = "true";
-            });
-            modalBackground.addEventListener("click", (event) => {
-                event.preventDefault();
-                if(event.target === modalBackground) {
-                    closeModal();
-                    modalBackground.ariaModal = "false";
-                    modalBackground.ariaHidden = "true";
-                }
-            });
-            displayPhotosGallery();
-            displayAddPhotoForm();
-            const erreur = document.querySelector("#erreur");
-            erreur.innerText = "";
-            const galleryView = document.querySelector(".gallery-view");
-            const addView = document.querySelector(".add-view");
-            const title = document.getElementById("modal-title");
-            const button = document.getElementById("modal-button");
-            const back = document.querySelector(".icon-back");
-            const iconWrapper = document.getElementById("icon-wrapper");
-            iconWrapper.classList.add("icon-wrapper-top");
-            document.querySelector(".button-modal").addEventListener("click", () => {
-                iconWrapper.classList.remove("icon-wrapper-top");
-                back.style.display = "block";
-                galleryView.style.display = "none";
-                addView.style.display = "block";
-                title.innerText = "Ajout photo";
-                button.innerText = "Valider";
-                button.type = "submit";
-                const form = document.getElementById("modal-form");
-                form.appendChild(button);
-                button.classList.remove("selected");
-                button.classList.add("greyed");
-            });
-            document.querySelector(".icon-back").addEventListener("click", () => {
-                iconWrapper.classList.add("icon-wrapper-top");
-                back.style.display = "none";
-                galleryView.style.display = "grid";
-                addView.style.display = "none";
-                title.innerText = "Galerie photo";
-                button.innerText = "Ajouter une photo";
-                button.classList.remove("greyed");
-                button.classList.add("selected");
-            });
-            /****** Step 3.3 add work ******/
-            console.log("step3.3");
-            /*document.querySelector("#modal-form").addEventListener("submit", (event) => {
-                event.preventDefault();
-                console.log("step3.3 submit");
-                addSubmit();
-            });*/
+            }
         });
-    }
-});
+        displayPhotosGallery();
+        displayAddPhotoForm();
+
+        const erreur = document.querySelector("#erreur");
+        erreur.innerText = "";
+
+        const galleryView = document.querySelector(".gallery-view");
+        const addView = document.querySelector(".add-view");
+        const title = document.getElementById("modal-title");
+        const button = document.getElementById("modal-button");
+        const back = document.querySelector(".icon-back");
+        const iconWrapper = document.getElementById("icon-wrapper");
+        iconWrapper.classList.add("icon-wrapper-top");
+
+        document.querySelector(".button-modal").addEventListener("click", () => {
+            iconWrapper.classList.remove("icon-wrapper-top");
+            back.style.display = "block";
+            galleryView.style.display = "none";
+            addView.style.display = "block";
+            title.innerText = "Ajout photo";
+            button.innerText = "Valider";
+            button.type = "submit";
+            const form = document.getElementById("modal-form");
+            form.appendChild(button);
+            button.classList.remove("selected");
+            button.classList.add("greyed");
+        });
+
+        document.querySelector(".icon-back").addEventListener("click", () => {
+            iconWrapper.classList.add("icon-wrapper-top");
+            back.style.display = "none";
+            galleryView.style.display = "grid";
+            addView.style.display = "none";
+            title.innerText = "Galerie photo";
+            button.innerText = "Ajouter une photo";
+            button.classList.remove("greyed");
+            button.classList.add("selected");
+        });
+        /****** Step 3.3 add work ******/
+        console.log("step3.3");
+        /*document.querySelector("#modal-form").addEventListener("submit", (event) => {
+            event.preventDefault();
+            console.log("step3.3 submit");
+            addSubmit();
+        });*/
+    });
+}
