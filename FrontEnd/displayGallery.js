f5dd61997ec1948bf0cc88748d981023c177dc4e
Daisy <detcherry@gmail.com>
2025-01-07 16:49:32 +0100

    Update modal styling and gallery display logic

--- FrontEnd/displayGallery.js
+++ FrontEnd/displayGallery.js
@@ -6,6 +6,7 @@ export const displayGallery = (projects) => {
     gallery.classList.add("gallery");
     document.getElementById("portfolio").appendChild(gallery);
   }
+
   // Vider la galerie avant d'ajouter les nouveaux éléments
   gallery.innerHTML = "";
 
@@ -23,4 +24,23 @@ export const displayGallery = (projects) => {
     figure.appendChild(figCaption);
     gallery.appendChild(figure);
   });
+
+  // Vider la galerie dans la modale avant d'ajouter les images
+  const modalGallery = document.querySelector(".modal-gallery");
+  if (modalGallery) {
+    modalGallery.innerHTML = ""; // Vider la galerie modale avant de la remplir
+    projects.forEach((project) => {
+      const modalFigure = document.createElement("figure");
+      const modalImage = document.createElement("img");
+      modalImage.src = project.imageUrl;
+      modalImage.alt = project.title;
+
+      const modalCaption = document.createElement("figcaption");
+      modalCaption.textContent = project.title;
+
+      modalFigure.appendChild(modalImage);
+      modalFigure.appendChild(modalCaption);
+      modalGallery.appendChild(modalFigure);
+    });
+  }
 };
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,11 +1,13 @@
 import { setupPage } from "./homePageSetup.js";
 import { initGalleryContent } from "./gallery.js";
+import { initializeModal } from "./modal.js";
 
 // Fonction principale pour initialiser la page d'accueil
 const initPage = async () => {
   if (window.location.pathname === "/index.html") {
     setupPage(); // Configure la page
     await initGalleryContent(); // Charge et affiche la galerie
+    initializeModal();
   } else {
     console.log(
       "On n'est pas sur la page d'accueil, la galerie ne peut être affichée."
