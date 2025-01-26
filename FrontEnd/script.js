41dde4b0d71be872b72a87e12826051c8a031e69
toby <tobyfoster97@gmail.com>
2025-01-26 05:50:37 +0100

    added photo upload 🐧

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -16,6 +16,8 @@ let token = "";
 
 let loginlogoutbtn = 0 
 
+let fetchedimages = [];
+ 
 
 function rendernav(){
   const navbar = document.getElementById("navbar");
@@ -65,6 +67,11 @@ const loginout = document.getElementById("login");
 }
 function rendermain(){
   const main = document.getElementById("main");
+  main.innerHTML = '';
+  const blackoutdiv = document.createElement("div");
+  blackoutdiv.id = "blackout";
+  blackoutdiv.addClassName = "blackoutoff";
+  main.appendChild(blackoutdiv);
   const introduction = document.createElement("section");
   introduction.id = "introduction";
   main.appendChild(introduction);
@@ -155,7 +162,13 @@ async function fetchImages() {
     const url = `${BASE_URL}${works}`;
     const response = await fetch(url, { method: "GET" });
     const data = await response.json();
-    renderGallery(data);
+    if (loginlogoutbtn === 1){
+      renderGallerypopup(data);
+    }
+    else{
+      renderGallery(data);
+    }
+    fetchedimages = data;
 };
 function renderGallery(data) {
   const gallery = document.getElementById("gallery");
@@ -367,11 +380,20 @@ function renderEditormode(){
   edittext.textContent = "Mode édition";
   Editormode.appendChild(edittext);
 }
+function blackouton(){
+  const blackout = document.getElementById("blackout");
+  blackout.classList.remove("blackoutoff");
+  blackout.classList.add("blackouton");
+}
+function blackoutoff(){
+  const blackout = document.getElementById("blackout");
+  blackout.classList.remove("blackouton");
+  blackout.classList.add("blackoutoff");
+}
+
 function modifypopup (){
   const main = document.getElementById("main");
-  const blackout = document.createElement("div");
-  blackout.id = "blackout";
-  main.appendChild(blackout);
+  blackouton();
   const popup = document.createElement("div");
   popup.id = "popup";
   main.appendChild(popup);
@@ -394,6 +416,13 @@ function modifypopup (){
   const title = document.createElement("h3");
   title.textContent = "Galerie photo";
   popup.appendChild(title);
+  const gallerypopup = document.createElement("div");
+  gallerypopup.id = "gallerypopup";
+  popup.appendChild(gallerypopup);
+  renderGallerypopup();
+  const line = document.createElement("div");
+  line.id = "line";
+  popup.appendChild(line);
   const add = document.createElement("button");
   add.id = "add";
   add.innerText = "Ajouter une photo";
@@ -403,10 +432,240 @@ function modifypopup (){
 
   function addpopup(){
     title.textContent = "Ajout photo";
+    const back = document.createElement("div");
+    back.id = "back";
+    back.addEventListener("click", () => {
+      rendereditpage();
+      modifypopup();
+    });
+    popup.appendChild(back);
+    const backsvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    backsvg.setAttribute("width", "21");
+    backsvg.setAttribute("height", "18");
+    backsvg.setAttribute("viewBox", "0 0 21 18");
+    backsvg.setAttribute("fill", "black");
+    back.appendChild(backsvg);
+    const backpath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    backpath.setAttribute(
+      "d",
+      "M0.439478 7.94458C-0.146493 8.53055 -0.146493 9.48217 0.439478 10.0681L7.9399 17.5686C8.52587 18.1545 9.47748 18.1545 10.0635 17.5686C10.6494 16.9826 10.6494 16.031 10.0635 15.445L5.11786 10.5041H19.4999C20.3297 10.5041 21 9.83375 21 9.00402C21 8.17428 20.3297 7.50393 19.4999 7.50393H5.12255L10.0588 2.56303C10.6447 1.97706 10.6447 1.02545 10.0588 0.439478C9.47279 -0.146493 8.52118 -0.146493 7.93521 0.439478L0.43479 7.9399L0.439478 7.94458Z"
+    );
+    backsvg.appendChild(backpath);
+    gallerypopup.innerHTML = "";
+    gallerypopup.id = "addphoto";
+    const form = document.createElement("form");
+    form.id = "uploadform";
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      verifyupload(event);
+    });
+    gallerypopup.appendChild(form);
+    const background = document.createElement("div");
+    background.id = "background";
+    form.appendChild(background);
+    const emptyphoto = document.createElement("div");
+    emptyphoto.id = "emptyphoto";
+    background.appendChild(emptyphoto);
+    const svg2 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg2.setAttribute("width", "69");
+    svg2.setAttribute("height", "60");
+    svg2.setAttribute("viewBox", "0 0 69 60");
+    svg2.setAttribute("fill", "#B9C5CC");
+    emptyphoto.appendChild(svg2);
+    const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path2.setAttribute(
+      "d",
+      "M60.5517 6.88793C61.7228 6.88793 62.681 7.84612 62.681 9.01724V51.5768L62.0156 50.7118L43.9165 27.2894C43.3176 26.5042 42.3727 26.0517 41.3879 26.0517C40.4031 26.0517 39.4715 26.5042 38.8594 27.2894L27.8136 41.5824L23.7546 35.8998C23.1557 35.0614 22.1975 34.569 21.1595 34.569C20.1214 34.569 19.1632 35.0614 18.5644 35.9131L7.91783 50.8183L7.31896 51.6434V51.6034V9.01724C7.31896 7.84612 8.27715 6.88793 9.44827 6.88793H60.5517ZM9.44827 0.5C4.75048 0.5 0.93103 4.31945 0.93103 9.01724V51.6034C0.93103 56.3012 4.75048 60.1207 9.44827 60.1207H60.5517C65.2495 60.1207 69.069 56.3012 69.069 51.6034V9.01724C69.069 4.31945 65.2495 0.5 60.5517 0.5H9.44827ZM20.0948 26.0517C20.9337 26.0517 21.7644 25.8865 22.5394 25.5655C23.3144 25.2444 24.0186 24.7739 24.6118 24.1807C25.2049 23.5876 25.6755 22.8834 25.9965 22.1083C26.3175 21.3333 26.4828 20.5027 26.4828 19.6638C26.4828 18.8249 26.3175 17.9943 25.9965 17.2192C25.6755 16.4442 25.2049 15.74 24.6118 15.1468C24.0186 14.5537 23.3144 14.0831 22.5394 13.7621C21.7644 13.4411 20.9337 13.2759 20.0948 13.2759C19.2559 13.2759 18.4253 13.4411 17.6503 13.7621C16.8752 14.0831 16.171 14.5537 15.5779 15.1468C14.9847 15.74 14.5142 16.4442 14.1931 17.2192C13.8721 17.9943 13.7069 18.8249 13.7069 19.6638C13.7069 20.5027 13.8721 21.3333 14.1931 22.1083C14.5142 22.8834 14.9847 23.5876 15.5779 24.1807C16.171 24.7739 16.8752 25.2444 17.6503 25.5655C18.4253 25.8865 19.2559 26.0517 20.0948 26.0517Z"    );
+    svg2.appendChild(path2);
+    const uploadbtn = document.createElement("input");
+    uploadbtn.type = "file";
+    uploadbtn.id = "uploadbtn";
+    uploadbtn.htmlFor = "uploadform";
+    uploadbtn.accept = "image/png, image/jpeg";
+    background.appendChild(uploadbtn);
+    const labeluploadbtn = document.createElement("label");
+    labeluploadbtn.htmlFor = "uploadbtn";
+    labeluploadbtn.textContent = "+ Ajouter photo";
+    labeluploadbtn.id = "labeluploadbtn";
+    background.appendChild(labeluploadbtn);
+    const maxtext = document.createElement("p");
+    maxtext.textContent = "jpg, png : 4mo max";
+    background.appendChild(maxtext);
+    labeluploadbtn.addEventListener("change", (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        background.innerHTML = ""; // Clear previous content
+        const uploadimg = document.createElement("img");
+        uploadimg.src = URL.createObjectURL(file);
+        uploadimg.onload = () => URL.revokeObjectURL(uploadimg.src); // Revoke object URL
+        background.appendChild(uploadimg);
+      }
+    });
+    const filetitle  = document.createElement("label");
+    filetitle.htmlFor = "uploadform";
+    filetitle.textContent = "Titre";
+    form.appendChild(filetitle);
+    const inputfiletitle = document.createElement("input");
+    inputfiletitle.id = "inputfiletitle";
+    inputfiletitle.type = "text";
+    inputfiletitle.htmlFor = "uploadform";
+    form.appendChild(inputfiletitle);
+    const category = document.createElement("label");
+    category.htmlFor = "uploadform";
+    category.textContent = "Catégorie";
+    form.appendChild(category);
+    const inputcategory = document.createElement("select");
+    inputcategory.htmlFor = "uploadform";
+    inputcategory.id = "inputcategory";
+    const disabled = new Option("Veuillez choisir", "", true, true);
+    disabled.disabled = true;
+    inputcategory.options.add(disabled);
+    inputcategory.options.add(new Option("Objets"));
+    inputcategory.options.add(new Option("Appartement"));
+    inputcategory.options.add(new Option("Hôtel & Restaurant"));
+    form.appendChild(inputcategory);
+    const errormsg = document.createElement("p");
+    errormsg.id = "errormsg";
+    errormsg.textContent = "";
+    form.appendChild(errormsg);
+    popup.removeChild(add);
+    const valider = document.createElement("input");
+    valider.type = "submit";
+    valider.id = "valider";
+    valider.htmlFor = "uploadform";
+    valider.value = "Valider";
+    valider.addEventListener("click", () => verifyupload());
+    popup.appendChild(valider);
+
+
+
   }
 
 
+};
+
+function verifyupload() {
+  const title = document.getElementById("inputfiletitle").value;
+  const category = document.getElementById("inputcategory").value;
+  const file = document.getElementById("uploadbtn").files[0];
+  const errormsg = document.getElementById("errormsg");
+  let titleerror = "";
+  let fileerror = "";
+  let categoryerror = "";
+  if (title === "") {
+    titleerror = "remplir le titre";
+  }
+  if (!file) {
+    fileerror = "ajouter une photo";
+  }
+  if (category === "") {
+    categoryerror = "choisir une catégorie";
+  }
+  const errors = [fileerror, titleerror, categoryerror].filter((msg) => msg !== "");
+  if (errors.length > 0) {
+    if (errors.length === 1) {
+      errormsg.textContent = `Veuillez ${errors[0]}.`;
+    } else {
+      const lastError = errors.pop();
+      errormsg.textContent = `Veuillez ${errors.join(", ")} et ${lastError}.`;
+    }
+  } else {
+    upload(title, category, file);
+  }
 }
+async function upload(title, category, file) {
+  const url = `${BASE_URL}${works}`;
+  const categoryMap = {
+    "Objets": 1,
+    "Appartement": 2,
+    "Hôtel & Restaurant": 3,
+  };
+  const mappedCategory = categoryMap[category];
+  const formData = new FormData();
+
+  formData.append("image", file);
+  formData.append("title", title);
+  formData.append("category", mappedCategory);
+  
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      const success = document.getElementById("addphoto");
+      success.innerHTML = "Photo ajoutée";
+    } else {
+      console.error("Upload failed 1:", data);
+    }
+  } catch (error) {
+    console.error("Upload failed 2:", error);
+  }
+}
+
+function renderGallerypopup(){
+  const gallery = document.getElementById("gallerypopup");
+  gallery.innerHTML = "";
+  fetchedimages.forEach((item) => {
+    const popupimg = document.createElement("div");
+    popupimg.id = "popupimg";
+    const img = document.createElement("img");
+    img.src = item.imageUrl;
+    img.alt = item.title;
+    popupimg.appendChild(img);
+    gallery.appendChild(popupimg);
+    const trashcan = document.createElement("div");
+    trashcan.id = "trashcan";
+    trashcan.addEventListener("click", () => deleteimage(item.id));
+    popupimg.appendChild(trashcan);
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "9");
+    svg.setAttribute("height", "11");
+    svg.setAttribute("viewBox", "0 0 9 11");
+    svg.setAttribute("fill", "white");
+    trashcan.appendChild(svg);
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute(
+      "d",
+      "M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" 
+    );
+    svg.appendChild(path);
+  });
+};
+
+
+
+
+
+
+
+
+
+
+function deleteimage(id){
+//  fetch(`http://localhost:3000/api/works/${id}`, {
+//    method: "DELETE",
+//    headers: {
+//      Authorization: `Bearer ${token}`,
+//    },
+//  })
+//    .then((response) => response.json())
+//    .then((data) => {
+//      console.log(data);
+//      fetchedimages = fetchedimages.filter((item) => item.id !== id);
+//      renderGallerypopup();
+//    })
+//    .catch((error) => console.error(error));
+}
+
+
+
 
 
 function forgotenn(){
